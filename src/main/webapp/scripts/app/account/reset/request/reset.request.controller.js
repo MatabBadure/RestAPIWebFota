6fcb38cf8c39ac79fe37ed20a4ccc79cb7fbb323
Ravi Ranjan <Ravi.Ranjan@razorfish.com>
2015-07-03 16:52:28 +0530

    login link in main.html and captcha in reset password WIP.

--- src/main/webapp/scripts/app/account/reset/request/reset.request.controller.js
+++ src/main/webapp/scripts/app/account/reset/request/reset.request.controller.js
@@ -1,25 +1,36 @@
 'use strict';
 
 angular.module('hillromvestApp')
-    .controller('RequestResetController', function ($rootScope, $scope, $state, $timeout, Auth) {
+    .controller('RequestResetController', function ($rootScope, $scope, $state, $timeout, Auth, localStorageService) {
 
         $scope.success = null;
         $scope.error = null;
         $scope.errorEmailNotExists = null;
         $scope.resetAccount = {};
+        $scope.user = {};
         $timeout(function (){angular.element('[ng-model="resetAccount.email"]').focus();});
 
         $scope.requestReset = function () {
-
+        	console.log($scope.form.valid);
+        	event.preventDefault();
             $scope.error = null;
             $scope.errorEmailNotExists = null;
+            $scope.showCaptcha = false;
 
             Auth.resetPasswordInit($scope.resetAccount.email).then(function () {
                 $scope.success = 'OK';
+                localStorage.setItem('passResetCount',0);
             }).catch(function (response) {
                 $scope.success = null;
                 if (response.status === 400 && response.data === 'e-mail address not registered') {
                     $scope.errorEmailNotExists = 'ERROR';
+                    
+                    
+                    var passResetCount = parseInt(localStorage.getItem('passResetCount')) || 0;
+                    localStorage.setItem('passResetCount', passResetCount + 1);
+                    if(passResetCount > 2){
+                    	$scope.showCaptcha = true;
+                    }
                 } else {
                     $scope.error = 'ERROR';
                 }

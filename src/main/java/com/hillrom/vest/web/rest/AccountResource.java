17c58e965968628f90a2b554cd86fcea0b3ef98b
rishabhjain-neev <Neevtech@123>
2015-07-07 18:50:18 +0530

    updated response of forgot password.

--- src/main/java/com/hillrom/vest/web/rest/AccountResource.java
+++ src/main/java/com/hillrom/vest/web/rest/AccountResource.java
@@ -1,5 +1,30 @@
 package com.hillrom.vest.web.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import net.minidev.json.JSONObject;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.codahale.metrics.annotation.Timed;
 import com.hillrom.vest.domain.Authority;
 import com.hillrom.vest.domain.User;
@@ -8,21 +33,6 @@ import com.hillrom.vest.security.SecurityUtils;
 import com.hillrom.vest.service.MailService;
 import com.hillrom.vest.service.UserService;
 import com.hillrom.vest.web.rest.dto.UserDTO;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * REST controller for managing the current user's account.
@@ -154,32 +164,37 @@ public class AccountResource {
         method = RequestMethod.POST,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    public ResponseEntity<?> requestPasswordReset(@RequestBody Map<String, String> body, HttpServletRequest request) {
-        
+    public ResponseEntity<JSONObject> requestPasswordReset(@RequestBody Map<String, String> body, HttpServletRequest request) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("message", "e-mail address not registered");
         return userService.requestPasswordReset(body.get("email"))
-            .map(user -> {
-                String baseUrl = request.getScheme() +
-                    "://" +
-                    request.getServerName() +
-                    ":" +
-                    request.getServerPort();
-            mailService.sendPasswordResetMail(user, baseUrl);
-            return new ResponseEntity<>("e-mail sent successfully.", HttpStatus.OK);
-            }).orElse(new ResponseEntity<>("e-mail address not registered", HttpStatus.BAD_REQUEST));
-        
+        		.map(user -> {
+        			String baseUrl = request.getScheme() +
+        					"://" +
+        					request.getServerName() +
+        					":" +
+        					request.getServerPort();
+        			mailService.sendPasswordResetMail(user, baseUrl);
+        			jsonObject.put("message", "e-mail sent successfully.");
+        			return ResponseEntity.ok().body(jsonObject);
+               }).orElse(ResponseEntity.badRequest().body(jsonObject));
     }
 
     @RequestMapping(value = "/account/reset_password/finish",
         method = RequestMethod.POST,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    public ResponseEntity<String> finishPasswordReset(@RequestParam(value = "key") String key, @RequestBody(required=true) Map<String,String> body) {
-    	String newPassword = body.get("newPassword");
+    public ResponseEntity<JSONObject> finishPasswordReset(@RequestParam(value = "key") String key, @RequestBody(required=true) Map<String,String> body) {
+        String newPassword = body.get("newPassword");
+        JSONObject jsonObject = new JSONObject();
         if (!checkPasswordLength(newPassword)) {
-            return new ResponseEntity<>("Incorrect password", HttpStatus.BAD_REQUEST);
+        	jsonObject.put("message", "Incorrect password");
+            return ResponseEntity.badRequest().body(jsonObject);
         }
-        return userService.completePasswordReset(newPassword, key)
-              .map(user -> new ResponseEntity<String>("Password reset successfully.", HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+        return userService.completePasswordReset(newPassword, key)          
+        		.map(user -> { 
+        			jsonObject.put("message", "Password reset successfully.");
+        			return ResponseEntity.ok().body(jsonObject);}).orElse(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
     }
 
     private boolean checkPasswordLength(String password) {

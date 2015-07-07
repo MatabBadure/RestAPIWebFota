52b1cfe15f500aad1c5e3d7e4fba3f2fe12c4a7a
Ravi Ranjan <Ravi.Ranjan@razorfish.com>
2015-07-07 14:11:11 +0530

    Forgot password Integration

--- src/main/webapp/scripts/app/account/reset/request/reset.request.controller.js
+++ src/main/webapp/scripts/app/account/reset/request/reset.request.controller.js
@@ -7,34 +7,77 @@ angular.module('hillromvestApp')
         $scope.error = null;
         $scope.errorEmailNotExists = null;
         $scope.resetAccount = {};
+        $scope.showCaptcha = false;
         $scope.user = {};
         $timeout(function (){angular.element('[ng-model="resetAccount.email"]').focus();});
 
         $scope.requestReset = function () {
         	console.log($scope.form.valid);
         	event.preventDefault();
-            $scope.error = null;
-            $scope.errorEmailNotExists = null;
-            $scope.showCaptcha = false;
-
-            Auth.resetPasswordInit($scope.resetAccount.email).then(function () {
-                $scope.success = 'OK';
-                localStorage.setItem('passResetCount',0);
-            }).catch(function (response) {
-                $scope.success = null;
-                if (response.status === 400 && response.data === 'e-mail address not registered') {
-                    $scope.errorEmailNotExists = 'ERROR';
-                    
-                    
-                    var passResetCount = parseInt(localStorage.getItem('passResetCount')) || 0;
-                    localStorage.setItem('passResetCount', passResetCount + 1);
-                    if(passResetCount > 2){
-                    	$scope.showCaptcha = true;
-                    }
-                } else {
-                    $scope.error = 'ERROR';
-                }
-            });
+        	  /*if ($scope.showCaptcha) {
+        		  //
+        		  var captchaData = $scope.user.captcha.response;
+        		  $.ajax({
+        		        type: "POST",
+        		        url: "https://www.google.com/recaptcha/api/siteverify",
+        		        dataType: 'json',
+        		        crossDomain: true,
+        		        data:  {
+        	                    'secret': '6LfwMAkTAAAAAHnNpBlH7fEixBPQBqLffYfArQ0E',
+        	                    'response': captchaData,
+        	                    'remoteip' :'10.132.161.102'
+        	                },
+        		        success: function (jsonResult) {
+        		           //do what ever with the reply
+        		        	console.log(jsonResult);
+        		        },
+        		        error: function (jqXHR, textStatus) {
+        		            //handle error
+        		        	console.log(jqXHR);
+        		        }
+        		    });
+        		  
+        		  
+        		  //
+               Auth.captcha($scope.user.captcha).then(function (data) {
+                 console.log(data)
+                 Auth.resetPasswordInit($scope.resetAccount.email).then(function () {
+                     $scope.success = 'OK';
+                     localStorage.setItem('passResetCount',0);
+                 }).catch(function (response) {
+                     $scope.success = null;
+                     if (response.status === 400 && response.data === 'e-mail address not registered') {
+                         $scope.errorEmailNotExists = 'ERROR';
+                         var passResetCount = parseInt(localStorage.getItem('passResetCount')) || 0;
+                         localStorage.setItem('passResetCount', passResetCount + 1);
+                         if(passResetCount > 2){
+                         	$scope.showCaptcha = true;	 
+                         }
+                     } else {
+                         $scope.error = 'ERROR';
+                     }
+                 });
+               }).catch(function (err) {
+                 console.log('ERROR :::',err)
+               });
+             }else*/
+            	 Auth.resetPasswordInit($scope.resetAccount.email).then(function () {
+                     $scope.success = 'OK';
+                     localStorage.setItem('passResetCount',0);
+                 }).catch(function (response) {
+                     $scope.success = null;
+                     if (response.status === 400 && response.data === 'e-mail address not registered') {
+                         $scope.errorEmailNotExists = 'ERROR';
+                         var passResetCount = parseInt(localStorage.getItem('passResetCount')) || 0;
+                         localStorage.setItem('passResetCount', passResetCount + 1);
+                         if(passResetCount > 2){
+                         	$scope.showCaptcha = true;	 
+                         }
+                     } else {
+                         $scope.error = 'ERROR';
+                     }
+                 });
+        	
         }
 
     });

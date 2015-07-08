7f1aa0a75dea4d98c09749230ae7ddad97f0a295
Ravi Ranjan <Ravi.Ranjan@razorfish.com>
2015-07-08 15:11:08 +0530

    added doctors list

--- src/main/webapp/scripts/components/entities/doctors/new/doctordirective.js
+++ src/main/webapp/scripts/components/entities/doctors/new/doctordirective.js
@@ -6,6 +6,30 @@ angular.module('hillromvestApp')
       templateUrl: 'scripts/components/entities/doctors/new/create.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
+      },
+      scope: {},
+      controller: function($scope){
+    	  $scope.doctor = {};
+    	  $scope.createDoctor = function(){
+    		  var data ={
+    				  'clinic' : $scope.doctor.clinicName,
+    				  'firstName' : $scope.doctor.firstName,
+    				  'firstName' : $scope.doctor.firstName,
+    				  'lastName' : $scope.doctor.lastName,
+    				  'speciality' : $scope.doctor.speciality,
+    				  'credentials' : $scope.doctor.credentials,
+    				  'email' : $scope.doctor.email,
+    				  'primaryPhone' : $scope.doctor.primaryPhone,
+    				  'mobilePhone' : $scope.doctor.mobilePhone,
+    				  'faxNumber' : $scope.doctor.faxNumber,
+    				  'address' : $scope.doctor.address,
+    				  'zipCode' : $scope.doctor.zipCode,
+    				  'city' : $scope.doctor.city,
+    				  'state' : $scope.doctor.state
+    				  
+    		  }
+    		  console.log("doctor form info", data);
+    	  }
       }
     };
   });

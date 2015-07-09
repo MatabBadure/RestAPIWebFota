1aae25237caa59ec37d0e2ef2dbaf866473537e7
Gopakumar <gopakumar.kondayin@gmail.com>
2015-07-09 17:58:43 +0530

    Fixed api call and updated secret key

--- src/main/java/com/hillrom/vest/web/rest/RecaptchaResource.java
+++ src/main/java/com/hillrom/vest/web/rest/RecaptchaResource.java
@@ -8,8 +8,6 @@ import javax.inject.Inject;
 import net.minidev.json.JSONObject;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +20,7 @@ import com.hillrom.vest.service.RecaptchaService;
 @RequestMapping("/api")
 public class RecaptchaResource {
 
+	private static final String EQUALTO = "=";
 	private static final String SECRET_KEY = "secret";
 	private static final String RESPONSE_KEY = "response";
 	private static final String RECAPTCHA_API_SITEVERIFY = "https://www.google.com/recaptcha/api/siteverify";
@@ -31,10 +30,17 @@ public class RecaptchaResource {
 	@RequestMapping(value="/recaptcha",method=RequestMethod.POST)
 	public ResponseEntity<JSONObject> processRecaptchaVerification(@RequestBody String response)throws URISyntaxException{
 		RestTemplate restClient = new RestTemplate();
-		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-		map.add(RESPONSE_KEY, response);
-		map.add(SECRET_KEY, recaptchaService.getRecaptchaPrivateKey());
-		return ResponseEntity.ok().body(restClient.postForObject(new URI(RECAPTCHA_API_SITEVERIFY), map,JSONObject.class));
+		StringBuilder uriBuilder = buildUri(response);
+		return ResponseEntity.ok().body(restClient.postForObject(new URI(uriBuilder.toString()), null,JSONObject.class));
+	}
+
+	private StringBuilder buildUri(String response) {
+		StringBuilder uriBuilder = new StringBuilder();
+		uriBuilder.append(RECAPTCHA_API_SITEVERIFY).append("?")
+				.append(RESPONSE_KEY).append(EQUALTO).append(response)
+				.append("&").append(SECRET_KEY).append(EQUALTO)
+				.append(recaptchaService.getRecaptchaPrivateKey());
+		return uriBuilder;
 	}
 
 }

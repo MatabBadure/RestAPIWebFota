c24c9a15cfbe7b9bc43cd8fb921e333e57912f28
SyedMdAli <syed.ali@razorfish.com>
2015-07-13 13:59:28 +0530

    Added Constant file for angular

---
+++ src/main/webapp/scripts/app/globalconfig.js
@@ -0,0 +1,15 @@
+'use strict';
+/* jshint unused:false */
+
+/**
+ * @ngdoc overview
+ * @name hillromvestApp
+ * @description
+ *
+ * Main configuration of the application.
+ */
+
+angular.module('hillromvestApp').value('globalConfig', {
+  locale: 'en-US',
+
+});

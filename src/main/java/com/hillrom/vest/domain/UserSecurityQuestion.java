6d58a3851f8a9c7e713786c14f9db8ea4f99fee5
Gopakumar <gopakumar.kondayin@gmail.com>
2015-07-16 17:43:04 +0530

    Generated SecurityQuestion Entity and test cases

---
+++ src/main/java/com/hillrom/vest/domain/UserSecurityQuestion.java
@@ -0,0 +1,58 @@
+package com.hillrom.vest.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USER_SECURITY_QUESTION")
+public class UserSecurityQuestion {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "USER_ID") 
+	private User user;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "QUESTION_ID") 
+	private SecurityQuestion securityQuestion;
+	
+	@Column
+	private String answer;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public SecurityQuestion getSecurityQuestion() {
+		return securityQuestion;
+	}
+	public void setSecurityQuestion(SecurityQuestion securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+	
+
+}
---
+++ src/test/java/com/hillrom/vest/service/SecurityQuestionServiceTest.java
@@ -0,0 +1,78 @@
+package com.hillrom.vest.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.hillrom.vest.Application;
+import com.hillrom.vest.domain.SecurityQuestion;
+import com.hillrom.vest.repository.SecurityQuestionRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest
+@Transactional
+public class SecurityQuestionServiceTest {
+	
+	private static final String SECURITY_QUESTION = "What is your favorite programming language?";
+	
+	@Inject
+	private SecurityQuestionRepository questionRepository;
+	
+	@Test
+	public void createSecQuestionSuccessfully(){
+		SecurityQuestion question = new SecurityQuestion();
+		question.setQuestion(SECURITY_QUESTION);
+		questionRepository.save(question);
+		assertThat(questionRepository.save(question));
+		questionRepository.delete(question);
+	}
+	
+	@Test(expected = ConstraintViolationException.class)
+	public void failToCreateSecurityQuestion(){
+		SecurityQuestion question = new SecurityQuestion();
+		questionRepository.save(question);
+	}
+	
+	@Test
+	public void findSecurityQuestionSuccessfully(){
+		SecurityQuestion question = new SecurityQuestion();
+		question.setQuestion(SECURITY_QUESTION);
+		question = questionRepository.save(question);
+		assertThat(questionRepository.findOne(question.getId()));
+		questionRepository.delete(question);
+	}
+	
+	@Test
+	public void updateSecurityQuestionFailure(){
+		SecurityQuestion question = new SecurityQuestion();
+		question.setQuestion(SECURITY_QUESTION);
+		question = questionRepository.save(question);
+		assertThat(questionRepository.findOne(question.getId()));
+		question.setQuestion(null);
+		questionRepository.save(question);
+		questionRepository.delete(question);
+	}
+	
+	@Test
+	public void updateSecurityQuestionSuccess(){
+		SecurityQuestion question = new SecurityQuestion();
+		question.setQuestion(SECURITY_QUESTION);
+		question = questionRepository.save(question);
+		assertThat(questionRepository.findOne(question.getId()));
+		question.setQuestion(SECURITY_QUESTION);
+		questionRepository.save(question);
+		questionRepository.delete(question);
+	}
+
+}

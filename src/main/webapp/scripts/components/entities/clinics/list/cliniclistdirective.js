f3646be2f7f608f08844d6cc626ee8489f1cbb28
SyedMdAli <syed.ali@razorfish.com>
2015-07-20 16:25:45 +0530

    Clinic Creation Changes

--- src/main/webapp/scripts/components/entities/clinics/list/cliniclistdirective.js
+++ src/main/webapp/scripts/components/entities/clinics/list/cliniclistdirective.js
@@ -1,77 +1,77 @@
 'use strict';
 
 angular.module('hillromvestApp')
-  .directive('clinicList', function() {
-      return {
-        templateUrl: 'scripts/components/entities/clinics/list/list.html',
-        restrict: 'E',
-        link: function postLink(scope, element, attrs) {},
-        scope: {
-          onSelect: '&'
-        },
-        link: function(scope, element, attrs) {
-          var clinic = scope.clinic;
-        },
-        controller: function($scope) {
-          $scope.clinics = [];
+  .directive('clinicList', function () {
+    return {
+      templateUrl: 'scripts/components/entities/clinics/list/list.html',
+      restrict: 'E',
+      scope: {
+        onSelect: '&'
+      },
+      link: function (scope) {
+        var clinic = scope.clinic;
+      },
+      controller: function ($scope) {
+        $scope.clinics = [];
 
-          $scope.selectClinic = function(clinic) {
-            $scope.clinic = clinic;
-            $scope.onSelect({
-              'clinic': clinic
-            });
-          };
-
-          $scope.searchClinics = function() {
-            $scope.clinics = [{
-                "id": 64,
-                "name": "Hill Rom",
-                "address": "Neev",
-                "zipcode": 560042,
-                "city": "Bangalore",
-                "state": "Karnataka",
-                "phoneNumber": 9740353872,
-                "faxNumber": 9942354883,
-                "hillromId": null,
-                "parentClinic": null,
-                "npiNumber": null,
-                "users": [],
-                "patients": [],
-                "deleted": false,
-                "childClinics": [{
-                  "id": 65,
-                  "name": "abc"
-                }, {
-                  "id": 66,
-                  "name": "xyz"
-                }, {
-                  "id": 67,
-                  "name": "pqrs"
-                }, {
-                  "id": 68,
-                  "name": "qwerty"
-                }]
-              }, {
-                "id": 69,
-                "name": "Neevtech",
-                "address": "RazorFish",
-                "zipcode": 560048,
-                "city": "bangalore",
-                "state": "karnataka",
-                "phoneNumber": 9740932492,
-                "faxNumber": null,
-                "hillromId": null,
-                "parentClinic": null,
-                "npiNumber": null,
-                "users": [],
-                "patients": [],
-                "deleted": false,
-                "childClinics": [{
-                    "id": 70,
-                    "name": "12345678"
-                  }]
-                }];
-            }
-          }
+        $scope.selectClinic = function (clinic) {
+          $scope.clinic = clinic;
+          $scope.onSelect({
+            'clinic': clinic
+          });
         };
-      });
+
+        $scope.searchClinics = function () {
+          $scope.clinics = [{
+            "id": 64,
+            "name": "Hill Rom",
+            "address": "Neev",
+            "zipcode": 560042,
+            "city": "Bangalore",
+            "state": "Karnataka",
+            "phoneNumber": 9740353872,
+            "faxNumber": 9942354883,
+            "hillromId": null,
+            "parentClinic": null,
+            "npiNumber": null,
+            "users": [],
+            "patients": [],
+            "deleted": false,
+            "childClinics": [{
+              "id": 65,
+              "name": "abc"
+            }, {
+              "id": 66,
+              "name": "xyz"
+            }, {
+              "id": 67,
+              "name": "pqrs"
+            }, {
+              "id": 68,
+              "name": "qwerty"
+            }]
+          }, {
+            "id": 69,
+            "name": "Neevtech",
+            "parent":"Razorfish",
+            "address": "RazorFish",
+            "zipcode": 560048,
+            "city": "bangalore",
+            "state": "karnataka",
+            "phoneNumber": 9740932492,
+            "faxNumber": null,
+            "hillromId": null,
+            "parentClinic": null,
+            "npiNumber": null,
+            "users": [],
+            "patients": [],
+            "deleted": false,
+            "childClinics": [{
+              "id": 70,
+              "name": "12345678"
+            }]
+          }];
+        };
+      }
+    };
+  });
--- src/main/webapp/scripts/components/entities/clinics/new/clinicdirective.js
+++ src/main/webapp/scripts/components/entities/clinics/new/clinicdirective.js
@@ -1,89 +1,148 @@
-  'use strict';
+'use strict';
 
-  angular.module('hillromvestApp')
-    .directive('clinic', function(ClinicService) {
-      return {
-        templateUrl: 'scripts/components/entities/clinics/new/create.html',
-        restrict: 'E',
-        scope: {
-          clinic: '=clinicData',
-          clinicStatus: '=clinicStatus'
-        },
-        link: function postLink(scope, element, attrs) {},
+angular.module('hillromvestApp')
+  .directive('clinic', function (ClinicService) {
+    return {
+      templateUrl: 'scripts/components/entities/clinics/new/create.html',
+      restrict: 'E',
+      scope: {
+        clinic: '=clinicData',
+        clinicStatus: '=clinicStatus'
+      },
+      controller: function ($scope) {
+        $scope.init = function () {
+          $scope.clinic = {};
+          $scope.clinic.type = 'parent';
+        };
 
-        controller: function($scope) {
-          $scope.init = function() {
-            $scope.clinic = {};
-            $scope.clinic.childClinics = [{
-              name: ''
-            }];
+        $scope.init();
+        $scope.newChildClinic = function () {
+          $scope.clinic.childClinics.push({
+            name: ''
+          });
+        };
+
+        $scope.removeChildClinic = function (idx) {
+          $scope.clinic.childClinics.splice(idx, 1);
+        };
+
+        $scope.clinicFormSubmit = function () {
+          if ($scope.form.$invalid) {
+            return false;
           }
+          if ($scope.clinicStatus.isCreate) {
+            $scope.clinic.hillromId = null;
+            var data = $scope.clinic;
 
-          $scope.init();
-          $scope.newChildClinic = function() {
-            $scope.clinic.childClinics.push({
-              name: ''
-            });
-          };
+            console.info(data);
 
-          $scope.removeChildClinic = function(idx) {
-            $scope.clinic.childClinics.splice(idx, 1);
-          }
-
-          $scope.clinicFormSubmit = function() {
-            if ($scope.form.$invalid) {
-              return false;
-            }
-            if ($scope.clinicStatus.isCreate) {
-              $scope.clinic.hillromId = null;
-              var data = $scope.clinic;
-
-              ClinicService.createClinic(data).then(function(data) {
-                $scope.clinicStatus.isMessage = true;
-                $scope.clinicStatus.message = "Clinic created successfully" + " with ID " + data.data.ParentClinic.id;
-                $scope.init();
-                $scope.form.$setPristine();
-              }).catch(function(response) {
-                if (response.data.message !== undefined) {
-                  $scope.clinicStatus.message = response.data.message;
-                } else {
-                  $scope.clinicStatus.message = 'Error occurred! Please try again';
-                }
-                $scope.clinicStatus.isMessage = true;
-              });
-            } else {
-              $scope.clinic.hillromId = null;
-              var data = $scope.clinic;
-              ClinicService.updateClinic(data).then(function(data) {
-                $scope.clinicStatus.isMessage = true;
-                $scope.clinicStatus.message = "Clinic updated successfully" + " with ID " + data.data.ParentClinic.id;
-                $scope.init();
-                $scope.form.$setPristine();
-              }).catch(function(response) {
-                if (response.data.message !== undefined) {
-                  $scope.clinicStatus.message = response.data.message;
-                } else {
-                  $scope.clinicStatus.message = 'Error occurred! Please try again';
-                }
-                $scope.clinicStatus.isMessage = true;
-              });
-            }
-          };
-
-          $scope.deleteClinic = function() {
-            $scope.clinic.id = 1;
-            ClinicService.deleteClinic($scope.clinic.id).then(function(data) {
+            ClinicService.createClinic(data).then(function (data) {
               $scope.clinicStatus.isMessage = true;
-              $scope.clinicStatus.message = data.data.message;
-            }).catch(function(response) {
+              $scope.clinicStatus.message = "Clinic created successfully" + " with ID " + data.data.ParentClinic.id;
+              $scope.init();
+              $scope.form.$setPristine();
+            }).catch(function (response) {
               if (response.data.message !== undefined) {
-                $scope.clinicStatus.message = data.data.message;
+                $scope.clinicStatus.message = response.data.message;
               } else {
-                $scope.clinicStatus.message = 'Error occured! Please try again';
+                $scope.clinicStatus.message = 'Error occurred! Please try again';
               }
               $scope.clinicStatus.isMessage = true;
             });
-          };
-        }
-      };
-    });
+          } else {
+            $scope.clinic.hillromId = null;
+            var data = $scope.clinic;
+            ClinicService.updateClinic(data).then(function (data) {
+              $scope.clinicStatus.isMessage = true;
+              $scope.clinicStatus.message = "Clinic updated successfully" + " with ID " + data.data.ParentClinic.id;
+              $scope.init();
+              $scope.form.$setPristine();
+            }).catch(function (response) {
+              if (response.data.message !== undefined) {
+                $scope.clinicStatus.message = response.data.message;
+              } else {
+                $scope.clinicStatus.message = 'Error occurred! Please try again';
+              }
+              $scope.clinicStatus.isMessage = true;
+            });
+          }
+        };
+
+        $scope.deleteClinic = function () {
+          $scope.clinic.id = 1;
+          ClinicService.deleteClinic($scope.clinic.id).then(function (data) {
+            $scope.clinicStatus.isMessage = true;
+            $scope.clinicStatus.message = data.data.message;
+          }).catch(function (response) {
+            if (response.data.message !== undefined) {
+              $scope.clinicStatus.message = data.data.message;
+            } else {
+              $scope.clinicStatus.message = 'Error occured! Please try again';
+            }
+            $scope.clinicStatus.isMessage = true;
+          });
+        };
+
+        $scope.getParentClinic = function () {
+          $scope.clinics = [{
+            "id": 64,
+            "name": "Hill Rom",
+            "address": "Neev",
+            "zipcode": 560042,
+            "city": "Bangalore",
+            "state": "Karnataka",
+            "phoneNumber": 9740353872,
+            "faxNumber": 9942354883,
+            "hillromId": null,
+            "parentClinic": null,
+            "npiNumber": null,
+            "users": [],
+            "patients": [],
+            "deleted": false,
+            "childClinics": [{
+              "id": 65,
+              "name": "abc"
+            }, {
+              "id": 66,
+              "name": "xyz"
+            }, {
+              "id": 67,
+              "name": "pqrs"
+            }, {
+              "id": 68,
+              "name": "qwerty"
+            }]
+          }, {
+            "id": 69,
+            "name": "Neevtech",
+            "parent": "Razorfish",
+            "address": "RazorFish",
+            "zipcode": 560048,
+            "city": "bangalore",
+            "state": "karnataka",
+            "phoneNumber": 9740932492,
+            "faxNumber": null,
+            "hillromId": null,
+            "parentClinic": null,
+            "npiNumber": null,
+            "users": [],
+            "patients": [],
+            "deleted": false,
+            "childClinics": [{
+              "id": 70,
+              "name": "12345678"
+            }]
+          }];
+        };
+
+        $scope.selectClinic = function (clinic) {
+          $scope.clinic.parent = clinic.name;
+          $scope.clinics = [];
+        };
+
+        $scope.removeParent = function () {
+          $scope.clinic.parent = null;
+        };
+      }
+    };
+  });

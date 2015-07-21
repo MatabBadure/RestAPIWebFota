a8e10857c931368aa886c2a8d81d6dff93ee88ff
SyedMdAli <syed.ali@razorfish.com>
2015-07-21 12:17:32 +0530

    Correcting Test case

--- src/test/javascript/spec/app/entities/users/users.service.spec.js
+++ src/test/javascript/spec/app/entities/users/users.service.spec.js
@@ -1,37 +1,53 @@
 'use strict';
-describe('Service: User', function () {
+describe('Service: UserService', function() {
 
-  var User, httpBackend, http, q;
+  var UserService, httpBackend, http, q, data, usersURL, deleteURL, date, accountDetailsURL;
 
   beforeEach(module('hillromvestApp'));
 
 
-  beforeEach(inject(function (_User_, $httpBackend,  _$http_, _$q_) {
-    User = _User_;
+  beforeEach(inject(function(_UserService_, $httpBackend, _$http_, _$q_) {
+    UserService = _UserService_;
     http = _$http_;
     q = _$q_;
     httpBackend = $httpBackend;
+    data = {
+      'email': 'syedmohammadali+111@neevtech.com',
+      'firstName': 'Syed',
+      'lastName': 'Ali',
+      'middleName': 'Mohammad',
+      'role': 'SUPER_ADMIN',
+      'title': 'Mr.'
+    };
+    date = new Date('2015-01-01T00:00:00Z');
+    spyOn(window, 'Date').and.callFake(function() {
+      return date;
+    });
+    httpBackend.whenGET(/\.*.html/).respond({});
+    httpBackend.whenGET(/\.*.json/).respond({});
+    usersURL = 'api/hillromteamuser?cacheBuster=1420070400000';
+    accountDetailsURL = 'api/account?cacheBuster=1420070400000';
+    deleteURL = 'api/hillromteamuser/16?cacheBuster=1420070400000';
+    httpBackend.whenPOST(usersURL).respond(200, data);
+    httpBackend.whenGET(accountDetailsURL).respond(200, data);
+    httpBackend.whenDELETE(deleteURL).respond(200, data);
   }));
 
 
-  it('createUser', function () {
-    var data = {
-      'email' : 'syedmohammadali@neevtech.com',
-      'firstName' : 'Syed',
-      'lastName' : 'Ali',
-      'middleName' : 'Mohammad',
-      'role' : 'SUPER_ADMIN',
-      'title' : 'Mr.'
-    };
-    var response = User.createUser(data);
+  it('createUser', function() {
+    var response = UserService.createUser(data);
     httpBackend.flush();
-    expect(response.status).toEqual(201);
+    response.then(function(data) {
+      expect(response.status).toEqual(201);
+    });
   });
 
-  it('deleteUser', function () {
-    var id = '';
-    var response = User.deleteUser(id);
+  it('deleteUser', function() {
+    var id  = 16;
+    var response = UserService.deleteUser(id);
     httpBackend.flush();
-    expect(response.status).toEqual(200);
+    response.then(function(data){
+      expect(response.status).toEqual(200);
+    });
   });
 });

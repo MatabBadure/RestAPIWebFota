8ed7a77180ae696001ef26ec405059ad7189aab1
Ravi Ranjan <Ravi.Ranjan@razorfish.com>
2015-07-25 20:20:36 +0530

    adding pagination & services

--- src/main/webapp/scripts/components/entities/doctors/list/doctorslistdirective.js
+++ src/main/webapp/scripts/components/entities/doctors/list/doctorslistdirective.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('hillromvestApp')
-.directive('doctorList', function (UserService) {
+.directive('doctorList', function (UserService,DoctorService) {
   return {
     templateUrl: 'scripts/components/entities/doctors/list/list.html',
     restrict: 'E',
@@ -13,28 +13,56 @@ angular.module('hillromvestApp')
       var doctor = scope.doctor;
     },
     controller: function($scope) {
-      $scope.doctors =[];
+
+      $scope.init = function(){
+          $scope.doctors =[];
+          $scope.doctorInfo ={};
+          $scope.currentPageIndex = 1;
+          $scope.perPageCount = 10;
+          $scope.pageCount = 0;
+          $scope.total = 0;
+          $scope.noMatchFound = false;
+        };
+
+        $scope.init();
 
       $scope.selectDoctor = function(doctor) {
-        $scope.doctor = doctor;
-        console.info(doctor.id);
+        
         UserService.getUser(doctor.id).then(function (response) {
-          response.data.clinicList = [ { 'id': 1, 'name': 'Name1'}, { 'id': 2, 'name': 'Name2'}];
-          console.log('Complete Object: ', $scope.doctor);
-          $scope.doctor = response.data;
-          $scope.onSelect({'doctor': doctor});
-        }).catch(function (response) {
-
-        });
+            $scope.doctorInfo = response.data;  
+            $scope.doctor = $scope.doctorInfo;
+            $scope.onSelect({'doctor': $scope.doctor});
+            }).catch(function (response) {
+              
+           });
       };
 
        $scope.createDoctor = function(){
           $scope.onCreate();
         };
 
-      $scope.searchDoctors = function () {
-        $scope.doctors = doctorsList;
-      };
+      $scope.searchDoctors = function(track){
+          if(track!==undefined){
+            if(track === "PREV" && $scope.currentPageIndex >1)
+              $scope.currentPageIndex--;
+            if(track === "NEXT")
+              $scope.currentPageIndex++;
+          }
+          DoctorService.getDoctorsList($scope.searchItem,$scope.currentPageIndex,$scope.perPageCount)
+          .then(function (response) {
+              $scope.doctors = response.data;
+              $scope.total = response.headers()['x-total-count'];
+              $scope.pageCount = Math.floor($scope.total / 10)+1;
+              if($scope.total == 0){
+                $scope.noMatchFound = true;
+              }else{
+                $scope.noMatchFound = false;
+              }
+              }).catch(function (response) {
+              $scope.noMatchFound = true;
+         });
+        
+        } 
     }
   };
 });

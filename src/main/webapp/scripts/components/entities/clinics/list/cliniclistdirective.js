ca9daf30255d53037e64f35a1ee0a0d1fd252e54
SyedMdAli <syed.ali@razorfish.com>
2015-07-27 12:47:06 +0530

    Fix for Know issue in search of Manage User, Patient, Doctor and Clinic

--- src/main/webapp/scripts/components/entities/clinics/list/cliniclistdirective.js
+++ src/main/webapp/scripts/components/entities/clinics/list/cliniclistdirective.js
@@ -12,18 +12,36 @@ angular.module('hillromvestApp')
     link: function(scope) {
       var clinic = scope.clinic;
     },
-    controller: function($scope) {
+    controller: function($scope, $timeout) {
 
       $scope.init = function () {
         $scope.currentPageIndex = 1;
         $scope.perPageCount = 10;
         $scope.pageCount = 0;
         $scope.total = 0;
+        $scope.clinics = [];
       };
 
       $scope.init();
 
-      $scope.clinics = [];
+      var timer = false;
+      $scope.$watch('searchItem', function () {
+        if(timer){
+          $timeout.cancel(timer)
+        }
+        timer= $timeout(function () {
+          if ($scope.searchItem) {
+            ClinicService.getClinics($scope.searchItem, 1, 10).then(function (response) {
+            $scope.clinics = response.data;
+          }).catch(function (response) {
+
+          });
+          } else {
+            $scope.clinics = [];
+          }
+        },1000)
+      });
+
       $scope.selectClinic = function (clinic) {
         $scope.clinic = clinic;
         $scope.onSelect({

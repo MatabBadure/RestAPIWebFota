a0b402018ce0ced9ba7020f2c5d7fd9b19ec7b8a
Ravi Ranjan <Ravi.Ranjan@razorfish.com>
2015-07-28 10:04:41 +0530

    code refactoring!

--- src/main/webapp/scripts/components/navbar/navbar.directive.js
+++ src/main/webapp/scripts/components/navbar/navbar.directive.js
@@ -1,76 +1,70 @@
-'use strict';
+  'use strict';
 
-angular.module('hillromvestApp')
-    .directive('activeMenu', function($translate, $locale, tmhDynamicLocale) {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                var language = attrs.activeMenu;
+  angular.module('hillromvestApp')
+  .directive('activeMenu', function($translate, $locale, tmhDynamicLocale) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var language = attrs.activeMenu;
 
-                scope.$watch(function() {
-                    return $translate.use();
-                }, function(selectedLanguage) {
-                    if (language === selectedLanguage) {
-                        tmhDynamicLocale.set(language);
-                        element.addClass('active');
-                    } else {
-                        element.removeClass('active');
-                    }
-                });
-            }
-        };
-    })
-    .directive('activeLink', function(location) {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                var clazz = attrs.activeLink;
-                var path = attrs.href;
+        scope.$watch(function() {
+          return $translate.use();
+        }, function(selectedLanguage) {
+          if (language === selectedLanguage) {
+            tmhDynamicLocale.set(language);
+            element.addClass('active');
+          } else {
+            element.removeClass('active');
+          }
+        });
+      }
+    };
+  })
+  .directive('activeLink', function(location) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var clazz = attrs.activeLink;
+        var path = attrs.href;
                 path = path.substring(1); //hack because path does bot return including hashbang
                 scope.location = location;
                 scope.$watch('location.path()', function(newPath) {
-                    if (path === newPath) {
-                        element.addClass(clazz);
-                    } else {
-                        element.removeClass(clazz);
-                    }
+                  if (path === newPath) {
+                    element.addClass(clazz);
+                  } else {
+                    element.removeClass(clazz);
+                  }
                 });
-            }
-        };
-    });
+              }
+            };
+          });
 
 
-angular.module('hillromvestApp')
+  angular.module('hillromvestApp')
   .directive('navigationBar', function (Auth, Principal, $state, Account) {
     return {
       templateUrl: 'scripts/components/navbar/navbar.html',
       restrict: 'E',
 
       controller: function ($scope) {
-        Account.get().$promise
-            .then(function (account) {
-                $scope.isAuthenticated = true;
-                
-            })
-            .catch(function() {
-                $scope.isAuthenticated = false;
-            });
 
+        $scope.signOut = function(){
+          Account.get().$promise
+          .then(function (account) {
+            $scope.isAuthenticated = true;
+            
+          })
+          .catch(function() {
+            $scope.isAuthenticated = false;
+          });
+        }
+        $scope.signOut();
         $scope.logout = function(){
-            Auth.signOut().then(function(data) {
-              Auth.logout();
-               Account.get().$promise
-                    .then(function (account) {
-                        $scope.isAuthenticated = true;
-                        
-                    })
-                    .catch(function() {
-                        $scope.isAuthenticated = false;
-                        $state.go('login');
-                    });
-              
-            }).catch(function(err) {
-            });
+          Auth.signOut().then(function(data) {
+            Auth.logout();
+            $scope.signOut();
+          }).catch(function(err) {
+          });
         }
       }
     };

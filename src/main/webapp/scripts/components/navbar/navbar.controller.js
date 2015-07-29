819bfb684cbfe8a1f13a97c9768871fcc0b3bc94
Avnesh S <avnesh@neevtech.com>
2015-07-29 18:59:38 +0530

    Active tab UI Issue

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -3,12 +3,4 @@
 angular.module('hillromvestApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal) {
         $scope.$state = $state;
-        $scope.isActive = function(tab) {
-          var path = $location.path();
-          if (path.indexOf(tab) !== -1) {
-            return true;
-          } else {
-            return false;
-          }
-        };
     });

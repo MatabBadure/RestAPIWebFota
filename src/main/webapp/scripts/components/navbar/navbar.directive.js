4ed3e11a06170ab73a65802a830a805d451f4e57
Avnesh S <avnesh@neevtech.com>
2015-07-29 16:11:02 +0530

    Custom Directive for Popover

--- src/main/webapp/scripts/components/navbar/navbar.directive.js
+++ src/main/webapp/scripts/components/navbar/navbar.directive.js
@@ -1,72 +1,89 @@
-  'use strict';
+'use strict';
 
-  angular.module('hillromvestApp')
-  .directive('activeMenu', function($translate, $locale, tmhDynamicLocale) {
-    return {
-      restrict: 'A',
-      link: function (scope, element, attrs) {
-        var language = attrs.activeMenu;
+angular.module('hillromvestApp')
+.directive('activeMenu', function($translate, $locale, tmhDynamicLocale) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var language = attrs.activeMenu;
 
-        scope.$watch(function() {
-          return $translate.use();
-        }, function(selectedLanguage) {
-          if (language === selectedLanguage) {
-            tmhDynamicLocale.set(language);
-            element.addClass('active');
-          } else {
-            element.removeClass('active');
-          }
+      scope.$watch(function() {
+        return $translate.use();
+      }, function(selectedLanguage) {
+        if (language === selectedLanguage) {
+          tmhDynamicLocale.set(language);
+          element.addClass('active');
+        } else {
+          element.removeClass('active');
+        }
+      });
+    }
+  };
+})
+
+.directive('activeLink', function(location) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var clazz = attrs.activeLink;
+      var path = attrs.href;
+      path = path.substring(1); //hack because path does bot return including hashbang
+      scope.location = location;
+      scope.$watch('location.path()', function(newPath) {
+        if (path === newPath) {
+          element.addClass(clazz);
+        } else {
+          element.removeClass(clazz);
+        }
+      });
+    }
+  };
+});
+
+
+angular.module('hillromvestApp')
+.directive('navigationBar', function (Auth, Principal, $state, Account) {
+  return {
+    templateUrl: 'scripts/components/navbar/navbar.html',
+    restrict: 'E',
+
+    controller: function ($scope) {
+
+      $scope.signOut = function(){
+        Account.get().$promise
+        .then(function (account) {
+          $scope.isAuthenticated = true;
+
+        })
+        .catch(function() {
+          $scope.isAuthenticated = false;
+          $state.go('login');
         });
       }
-    };
-  })
-  .directive('activeLink', function(location) {
-    return {
-      restrict: 'A',
-      link: function (scope, element, attrs) {
-        var clazz = attrs.activeLink;
-        var path = attrs.href;
-                path = path.substring(1); //hack because path does bot return including hashbang
-                scope.location = location;
-                scope.$watch('location.path()', function(newPath) {
-                  if (path === newPath) {
-                    element.addClass(clazz);
-                  } else {
-                    element.removeClass(clazz);
-                  }
-                });
-              }
-            };
-          });
-
-
-  angular.module('hillromvestApp')
-  .directive('navigationBar', function (Auth, Principal, $state, Account) {
-    return {
-      templateUrl: 'scripts/components/navbar/navbar.html',
-      restrict: 'E',
-
-      controller: function ($scope) {
-
-        $scope.signOut = function(){
-          Account.get().$promise
-          .then(function (account) {
-            $scope.isAuthenticated = true;
-
-          })
-          .catch(function() {
-            $scope.isAuthenticated = false;
-            $state.go('login');
-          });
-        }
-        $scope.signOut();
-        $scope.logout = function(){
-          Auth.signOut().then(function(data) {
-            Auth.logout();
-            $scope.signOut();
-          }).catch(function(err) {  
-          });
-        }
+      $scope.signOut();
+      $scope.logout = function(){
+        Auth.signOut().then(function(data) {
+          Auth.logout();
+          $scope.signOut();
+        }).catch(function(err) {  
+        });
       }
-    };
-  });
+    }
+  };
+});
+
+angular.module('hillromvestApp')
+.directive('customPopover', function () {
+  return {
+    restrict: 'A',
+    template: '<span class="padding-right">Jose Rubert</span><span class="hillrom-icon icon-arrow-down"></span>',
+    link: function (scope, el, attrs) {
+      $(el).popover({
+          trigger: 'click',
+          html: true,
+          content: attrs.popoverHtml,
+          placement: attrs.popoverPlacement
+      });
+    }
+  };
+});

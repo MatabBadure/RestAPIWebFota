90243d4a602f60c73fb2b054370bcff39a53f5d9
Ravi Ranjan <Ravi.Ranjan@razorfish.com>
2015-07-30 16:08:51 +0530

    date & zipcode fix for patient creation and edit

--- src/main/webapp/scripts/app/entities/patients/patients.controller.js
+++ src/main/webapp/scripts/app/entities/patients/patients.controller.js
@@ -6,14 +6,16 @@ angular.module('hillromvestApp')
 	$scope.getAge = function(selectedDate){
 		var currentDate = new Date();
 		var selectedDate = selectedDate;
-		var diff = currentDate - selectedDate ;
-		var years = Math.floor(diff/(1000*60*60*24*365));
-		var age;
-		age = years +1;
-		if(diff < 0){
-			age = 0;
-		};
-		return age;
+		  var years = currentDate.getFullYear() - selectedDate.getFullYear();
+          var age = 0;
+          age = years;
+          if(years == 0){
+            age = 1;
+          }
+          if (years < 0) {
+            age = 0;
+          };
+          return age;
 	}
 	$scope.patient = {};
 
@@ -32,9 +34,9 @@ angular.module('hillromvestApp')
 		if(patient.dob !== null){
 			$scope.patient.age = $scope.getAge(new Date($scope.patient.dob))
 			var dateArr = $scope.patient.dob.split('-');
-			$scope.patient.formatedDOB = dateArr[1]+"/"+dateArr[2]+"/"+dateArr[0];
+			$scope.patient.formatedDOB = dateArr[1]+"/"+dateArr[2]+"/"+dateArr[0].slice(-2);
 		}
-		$scope.patient.zipcode = $scope.patient.zipCode;
+		$scope.patient.zipcode = $scope.patient.zipcode;
 
 	};
 

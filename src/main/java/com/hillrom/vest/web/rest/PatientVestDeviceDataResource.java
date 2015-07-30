435cd6d09d2e7d01988ce41a77ee3d0760357d99
gopakumark <gopakumar.kondayin@razorfish.com>
2015-07-30 14:37:27 +0530

    Provided api for VestDeviceData by patientId and fixed redundant records updation

--- src/main/java/com/hillrom/vest/web/rest/PatientVestDeviceDataResource.java
+++ src/main/java/com/hillrom/vest/web/rest/PatientVestDeviceDataResource.java
@@ -1,20 +1,26 @@
 package com.hillrom.vest.web.rest;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hillrom.vest.domain.PatientVestDeviceData;
+import com.hillrom.vest.repository.PatientVestDeviceDataRepository;
 import com.hillrom.vest.service.PatientVestDeviceDataService;
+import com.hillrom.vest.web.rest.util.PaginationUtil;
 
 @RestController
 @RequestMapping("/api")
@@ -23,11 +29,25 @@ public class PatientVestDeviceDataResource {
 	@Inject
 	private PatientVestDeviceDataService deviceDataService;
 	
+	@Inject
+	private PatientVestDeviceDataRepository deviceDataRepository;
+	
 	@RequestMapping(value = "/receiveData",
             method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> receiveData(HttpServletRequest request){
 		List<PatientVestDeviceData> deviceData =deviceDataService.save(request.getQueryString());
 		return new ResponseEntity(deviceData,HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value = "/patient/{id}/vestdevicedata",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<PatientVestDeviceData>> findByPatientId(@PathVariable Long id,
+			@RequestParam(value="page",required=false)Integer pageNo,
+			@RequestParam(value="per_page",required=false)Integer per_page) throws URISyntaxException{
+		Page<PatientVestDeviceData> page = deviceDataRepository.findLatest(id,PaginationUtil.generatePageRequest(pageNo, per_page));
+		HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/patient/"+id+"/vestdevicedata", pageNo, per_page);
+		return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
 	}
 }

ff0e3986768f0267a6d3fd26274fd6f1666b3d6f
Ravi Ranjan <Ravi.Ranjan@razorfish.com>
2015-08-01 23:53:17 +0530

    API url change for GET patient. Added filter for language. Display Id in clinics page

--- src/main/webapp/scripts/app/entities/patients/patients.controller.js
+++ src/main/webapp/scripts/app/entities/patients/patients.controller.js
@@ -33,16 +33,17 @@ angular.module('hillromvestApp')
 		$scope.patient = patient;
 		if(patient.dob !== null){
 			$scope.patient.age = $scope.getAge(new Date($scope.patient.dob))
-			var dateArr = $scope.patient.dob.split('-');
-			$scope.patient.formatedDOB = dateArr[1]+"/"+dateArr[2]+"/"+dateArr[0].slice(-2);
-            var _month = dateArr[1];
-            _month = _month.length > 1 ? _month : '0' + _month;
-            var _day = dateArr[2];
-            _day = _day.length > 1 ? _day : '0' + _day;
-            var _year = dateArr[0];
-            var dob = _month+"/"+_day+"/"+_year;
+			var _date = new Date($scope.patient.dob);
+      var _month = (_date.getMonth()+1).toString();
+      _month = _month.length > 1 ? _month : '0' + _month;
+      var _day = (_date.getDate()).toString();
+      _day = _day.length > 1 ? _day : '0' + _day;
+      var _year = (_date.getFullYear()).toString();
+      var dob = _month+"/"+_day+"/"+_year;
 			$scope.patient.dob = dob;
+			$scope.patient.formatedDOB = _month+"/"+_day+"/"+_year.slice(-2);
 		}
+		patient.language = patient.langKey;
 
 	};
 

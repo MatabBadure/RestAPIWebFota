5b548d603fa79e9628ee6ac0b6312e24b71244e9
SyedMdAli <syed.ali@razorfish.com>
2015-08-02 16:43:52 +0530

    Select Drop Down issue

--- src/main/webapp/scripts/components/entities/doctors/list/doctorslistdirective.js
+++ src/main/webapp/scripts/components/entities/doctors/list/doctorslistdirective.js
@@ -1,96 +1,90 @@
 'use strict';
 
 angular.module('hillromvestApp')
-.directive('doctorList', function (UserService) {
-  return {
-    templateUrl: 'scripts/components/entities/doctors/list/list.html',
-    restrict: 'E',
-    scope: {
-      onSelect: '&',
-      onCreate: '&',
-      doctorStatus: '=doctorStatus'
-    },
-    link: function(scope, element, attrs) {
-      var doctor = scope.doctor;
-      scope.$on('resetList', function () {
-        scope.searchDoctors();
-      })
-    },
+  .directive('doctorList', function(UserService) {
+    return {
+      templateUrl: 'scripts/components/entities/doctors/list/list.html',
+      restrict: 'E',
+      scope: {
+        onSelect: '&',
+        onCreate: '&',
+        doctorStatus: '=doctorStatus'
+      },
+      link: function(scope, element, attrs) {
+        var doctor = scope.doctor;
+        scope.$on('resetList', function() {
+          scope.searchDoctors();
+        })
+      },
 
-    controller: function($scope, $timeout) {
+      controller: function($scope, $timeout) {
 
-      $scope.init = function () {
-        $scope.doctors =[];
-        $scope.doctorInfo ={};
-        $scope.currentPageIndex = 1;
-        $scope.perPageCount = 10;
-        $scope.pageCount = 0;
-        $scope.total = 0;
-        $scope.noMatchFound = false;
-        $scope.sortOption = "";
-        $scope.showModal = false;
-        $scope.searchDoctors();
-      };
+        $scope.init = function() {
+          $scope.doctors = [];
+          $scope.doctorInfo = {};
+          $scope.currentPageIndex = 1;
+          $scope.perPageCount = 10;
+          $scope.pageCount = 0;
+          $scope.total = 0;
+          $scope.noMatchFound = false;
+          $scope.sortOption = "";
+          $scope.showModal = false;
+          $scope.searchDoctors();
+        };
 
 
-      var timer = false;
-      $scope.$watch('searchItem', function () {
-        if (timer) {
-          $timeout.cancel(timer)
-        }
-        timer = $timeout(function () {
+        var timer = false;
+        $scope.$watch('searchItem', function() {
+          if (timer) {
+            $timeout.cancel(timer)
+          }
+          timer = $timeout(function() {
             $scope.searchDoctors();
-        },1000)
-      });
+          }, 1000)
+        });
 
 
-      $scope.selectDoctor = function(doctor) {
+        $scope.selectDoctor = function(doctor) {
+          $scope.doctor = doctor;
+          var url = '/api/user/' + doctor.id + '/hcp';
+          UserService.getUser(doctor.id, url).then(function(response) {
+            $scope.doctor = response.data;
+            $scope.onSelect({
+              'doctor': doctor
+            });
+          }).catch(function(response) {});
+        };
 
-        $scope.doctor = doctor;
-        $scope.onSelect({'doctor': doctor});
-        //Todo:
-        // UserService.getUser(doctor.id).then(function (response) {
-        //   response.data.clinicList = [ { 'id': 1, 'name': 'Name1'}, { 'id': 2, 'name': 'Name2'}];
-        //   console.log('Complete Object: ', $scope.doctor);
-        //   $scope.doctor = response.data;
-        //   $scope.onSelect({'doctor': doctor});
-        // }).catch(function (response) {
-
-        // });
-      };
-
-       $scope.createDoctor = function(){
+        $scope.createDoctor = function() {
           $scope.onCreate();
         };
 
-      $scope.searchDoctors = function(track) {
-        if (track !== undefined) {
-          if (track === "PREV" && $scope.currentPageIndex > 1) {
-            $scope.currentPageIndex--;
-          }
-          else if (track === "NEXT" && $scope.currentPageIndex < $scope.pageCount)
-            {
+        $scope.searchDoctors = function(track) {
+          if (track !== undefined) {
+            if (track === "PREV" && $scope.currentPageIndex > 1) {
+              $scope.currentPageIndex--;
+            } else if (track === "NEXT" && $scope.currentPageIndex < $scope.pageCount) {
               $scope.currentPageIndex++;
-            }else{
+            } else {
               return false;
             }
-        }
-        var url = 'api/user/hcp/search?searchString=';
-        UserService.getUsers(url, $scope.searchItem, $scope.sortOption, $scope.currentPageIndex, $scope.perPageCount).then(function (response) {
-          $scope.doctors = response.data;
-          $scope.total = response.headers()['x-total-count'];
-          $scope.pageCount = Math.ceil($scope.total / 10);
-          if($scope.total == 0){
-            $scope.noMatchFound = true;
-          }else{
-            $scope.noMatchFound = false;
           }
-        }).catch(function(response) {
+          var url = 'api/user/hcp/search?searchString=';
+          UserService.getUsers(url, $scope.searchItem, $scope.sortOption, $scope.currentPageIndex, $scope.perPageCount).then(function(response) {
+            $scope.doctors = response.data;
+            $scope.total = response.headers()['x-total-count'];
+            $scope.pageCount = Math.ceil($scope.total / 10);
+            if ($scope.total == 0) {
+              $scope.noMatchFound = true;
+            } else {
+              $scope.noMatchFound = false;
+            }
+          }).catch(function(response) {
 
-        });
-      };
+          });
+        };
 
-      $scope.init();
-    }
-  };
-});
+        $scope.init();
+      }
+    };
+  });

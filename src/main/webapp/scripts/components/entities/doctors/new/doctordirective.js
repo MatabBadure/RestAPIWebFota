928f2a91408aa83d6b3073f1c02fc44b479985f4
Ravi Ranjan <Ravi.Ranjan@razorfish.com>
2015-08-04 16:43:19 +0530

    Added per_page=100 in api url to get complete clinic list

--- src/main/webapp/scripts/components/entities/doctors/new/doctordirective.js
+++ src/main/webapp/scripts/components/entities/doctors/new/doctordirective.js
@@ -37,7 +37,7 @@ angular.module('hillromvestApp')
         $scope.getParentClinics = function() {
           var timer = false;
           timer = $timeout(function() {
-            ClinicService.getAllClinics('/api/clinics?filter=deleted:false').then(function(response) {
+            ClinicService.getAllClinics('/api/clinics?per_page=100&filter=deleted:false').then(function(response) {
               $scope.clinics = response.data;
               angular.forEach($scope.clinics, function(clinic) {
                 if(clinic.city) {

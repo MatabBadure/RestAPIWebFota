8b32477e0f8d7147d1d5040529b650da1b1071e6
SyedMdAli <syed.ali@razorfish.com>
2015-08-12 20:16:28 +0530

    Overview and demographics

--- src/main/webapp/scripts/app/modules/admin/patient/controllers/patients.controller.js
+++ src/main/webapp/scripts/app/modules/admin/patient/controllers/patients.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('hillromvestApp')
-  .controller('patientsController', function($scope, $filter, $state, $stateParams, patientService, dateService) {
+  .controller('patientsController', function($scope, $filter, $state, $stateParams, patientService, dateService, notyService, UserService) {
 
     $scope.patient = {};
 
@@ -23,17 +23,45 @@ angular.module('hillromvestApp')
 
     $scope.switchPatientTab = function(status){
       $scope.patientTab = status;
+      console.log(status);
       $state.go(status, {'patientId': $stateParams.patientId});
+    };
+
+    $scope.setOverviewMode = function(patient){
+      console.log(patient);
+      $scope.patient = patient;
+      if (patient.dob !== null) {
+        $scope.patient.age = dateService.getAge(new Date($scope.patient.dob));
+        var _date = dateService.getDate($scope.patient.dob);
+        var _month = dateService.getMonth(_date.getMonth());
+        var _day = dateService.getDay(_date.getDate());
+        var _year = dateService.getYear(_date.getFullYear());
+        var dob = _month + "/" + _day + "/" + _year;
+        $scope.patient.dob = dob;
+        $scope.patient.formatedDOB = _month + "/" + _day + "/" + _year.slice(-2);
+      }
     };
 
     $scope.initPatientOverview = function(){
       $scope.patientTab = "patientEdit";
+      console.log('Coming Here : ', $stateParams.patientId);
+      $scope.getPatiendDetails($stateParams.patientId, $scope.setOverviewMode);
     };
 
+    $scope.initpatientDemographic = function(){
+      $scope.getPatiendDetails($stateParams.patientId, $scope.setEditMode);
+    };
     $scope.init = function() {
+      $scope.patientTab = "patientOverview";
       var currentRoute = $state.current.name;
-      if($state.current.name === 'patientUser'){
+      if($state.current.name === 'patientOverview'){
         $scope.initPatientOverview();
+      }
+      if($state.current.name === 'patientDemographic'){
+        $scope.initpatientDemographic();
+      }
+      if($state.current.name === 'patientClinics'){
+        $scope.initPatientClinics();
       }
       if ($state.current.name === 'patientEdit') {
         $scope.getPatiendDetails($stateParams.patientId, $scope.setEditMode);
@@ -74,6 +102,35 @@ angular.module('hillromvestApp')
       $scope.patient = {
         title: 'Mr.'
       };
+    };
+
+    $scope.formSubmit = function(){
+      $scope.submitted = true;
+      if($scope.form.$invalid){
+        return false;
+      }
+      var data = $scope.patient;
+      data.role = 'PATIENT';
+      UserService.editUser(data).then(function (response) {
+        if(response.status === 200) {
+          $scope.patientStatus.isMessage = true;
+          $scope.patientStatus.message = "Patient updated successfully";
+          notyService.showMessage($scope.patientStatus.message, 'success');
+        } else {
+          $scope.patientStatus.message = 'Error occured! Please try again';
+          notyService.showMessage($scope.patientStatus.message, 'warning');
+        }
+      }).catch(function (response) {
+        $scope.patientStatus.isMessage = true;
+        if (response.data.message !== undefined) {
+          $scope.patientStatus.message = response.data.message;
+        } else if(response.data.ERROR !== undefined) {
+          $scope.patientStatus.message = response.data.ERROR;
+        } else {
+          $scope.patientStatus.message = 'Error occured! Please try again';
+        }
+        notyService.showMessage($scope.patientStatus.message, 'warning');
+      });
     };
 
     $scope.init();

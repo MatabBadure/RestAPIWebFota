5a7877fe3a9d7c02f19f4b078cd326e3408f49ac
avnesh-neev <avnesh.nitk@gmail.com>
2015-08-17 18:08:58 +0530

    Active Tab Issue

--- src/main/webapp/scripts/app/modules/admin/patient/directives/patient-info/navbar/navbar.directive.js
+++ src/main/webapp/scripts/app/modules/admin/patient/directives/patient-info/navbar/navbar.directive.js
@@ -4,6 +4,16 @@ angular.module('hillromvestApp')
 .directive('adminPatientNavbar', function() {
   return {
       templateUrl: 'scripts/app/modules/admin/patient/directives/patient-info/navbar/navbar.html',
-      restrict: 'E'
+      restrict: 'E',
+      controller: function ($scope, $location) {
+        $scope.isActive = function(tab) {
+          var path = $location.path();
+          if (path.indexOf(tab) !== -1) {
+            return true;
+          } else {
+            return false;
+          }
+        };
+      }
     }
 });

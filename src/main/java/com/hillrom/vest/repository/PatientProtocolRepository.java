d7cda145b73bdaec45fbfdc3cc0987073945df4f
gopakumark <gopakumar.kondayin@razorfish.com>
2015-08-17 14:20:44 +0530

    Implemented Stored Procedure related code

--- src/main/java/com/hillrom/vest/repository/PatientProtocolRepository.java
+++ src/main/java/com/hillrom/vest/repository/PatientProtocolRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 
 import com.hillrom.vest.domain.PatientProtocolData;
 
@@ -12,4 +13,11 @@ public interface PatientProtocolRepository extends
 
 	@Query("from PatientProtocolData ppd where ppd.patient.id = ?1")
 	Optional<PatientProtocolData> findOneByPatientId(String patientId);
+	
+	/**
+	 * This returns hillromId of the protocol from stored procedure.
+	 * @return String hillromId
+	 */
+	@Procedure(outputParameterName="hillrom_id",procedureName="get_next_protocol_hillromid")
+	String id();
 }

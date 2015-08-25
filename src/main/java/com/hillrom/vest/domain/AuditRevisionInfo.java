3e605e15b80ae6367eef159fa2a4f88cc279ef16
rohkushw <rohangtr@gmail.com>
2015-08-25 10:20:52 +0530

    In Progress
     -Custom Revision Info Entity Added

---
+++ src/main/java/com/hillrom/vest/domain/AuditRevisionInfo.java
@@ -0,0 +1,31 @@
+package com.hillrom.vest.domain;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.envers.DefaultRevisionEntity;
+import org.hibernate.envers.RevisionEntity;
+
+import com.hillrom.vest.service.AuditRevisionListener;
+
+@Entity
+@Table(name = "AUDIT_REVISION_INFO")
+@RevisionEntity(AuditRevisionListener.class)
+public class AuditRevisionInfo extends DefaultRevisionEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7387549815500904487L;
+	
+	@Column(name = "user_id")
+	private String userId;
+
+	public String getAuditor() {
+		return userId;
+	}
+
+	public void setAuditor(String userId) {
+		this.userId = userId;
+	}
+}

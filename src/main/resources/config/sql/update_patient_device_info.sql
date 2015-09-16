6ab33d29bdb19b9f5381ba03c716032ab7fe273a
Rohan Kushwaha <rohangtr@gmail.com>
2015-09-16 16:02:56 +0530

    Change table name from lower case to upper case as dev db is case sensitive

--- src/main/resources/config/sql/update_patient_device_info.sql
+++ src/main/resources/config/sql/update_patient_device_info.sql
@@ -18,19 +18,19 @@ SET today_date = now();
 SET created_by = 'System';
 
 -- check if same serial number exists for the patient
-SELECT `id`, `serial_number` INTO temo_patient_info_id, temp_serial_number FROM `patient_info`
+SELECT `id`, `serial_number` INTO temo_patient_info_id, temp_serial_number FROM `PATIENT_INFO`
 		WHERE `id`= patient_id AND `serial_number`=pat_device_serial_number ;
 
 IF temp_serial_number IS NOT NULL THEN
 
 -- if serial number exists for patient, update patient_info and patient_vest_device_history tables 
 START TRANSACTION;
-	UPDATE `patient_info` SET
+	UPDATE `PATIENT_INFO` SET
 	`hub_id` = pat_hub_id,
 	`bluetooth_id` = pat_bluetooth_id
 	WHERE `id` = patient_id;
     
-    UPDATE `patient_vest_device_history` SET
+    UPDATE `PATIENT_VEST_DEVICE_HISTORY` SET
 	`bluetooth_id` = pat_bluetooth_id,
 	`hub_id` = pat_hub_id,
 	`last_modified_date` = today_date
@@ -46,7 +46,7 @@ ELSE
   
   -- update patient_info table 
   
-	UPDATE `patient_info` SET
+	UPDATE `PATIENT_INFO` SET
 	`hub_id` = pat_hub_id,
 	`serial_number` = pat_device_serial_number,
 	`bluetooth_id` = pat_bluetooth_id,
@@ -54,12 +54,12 @@ ELSE
     
     -- make other devices inactive in patient_vest_device_history tables 
     
-	UPDATE `patient_vest_device_history` SET
+	UPDATE `PATIENT_VEST_DEVICE_HISTORY` SET
 	`is_active` = 0 WHERE `patient_id` = patient_id;
     
      -- make insert device into patient_vest_device_history with active.
      
-    INSERT INTO `patient_vest_device_history`
+    INSERT INTO `PATIENT_VEST_DEVICE_HISTORY`
 		(`patient_id`, `serial_number`,	`bluetooth_id`,	`hub_id`, `created_by`, `created_date`, `last_modified_by`, `last_modified_date`, `is_active`)
 		VALUES
 		(patient_id,pat_device_serial_number, pat_bluetooth_id,pat_hub_id,created_by,today_date,created_by,today_date,1);

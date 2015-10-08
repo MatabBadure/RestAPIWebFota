8703a0874aefba27bf7be428caeea148d45393f4
Rohan Kushwaha <rohangtr@gmail.com>
2015-10-08 16:21:53 +0530

    Changes in Revision Class

--- src/main/java/com/hillrom/vest/domain/AuditRevisionInfoEntity.java
+++ src/main/java/com/hillrom/vest/domain/AuditRevisionInfoEntity.java
@@ -1,22 +1,33 @@
 package com.hillrom.vest.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
+import org.hibernate.envers.RevisionNumber;
+import org.hibernate.envers.RevisionTimestamp;
 
 import com.hillrom.vest.service.AuditRevisionListener;
 
 @Entity
 @Table(name = "AUDIT_REVISION_INFO")
 @RevisionEntity(AuditRevisionListener.class)
-public class AuditRevisionInfoEntity extends DefaultRevisionEntity {
+public class AuditRevisionInfoEntity {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7387549815500904487L;
+	
+	@Id
+    @GeneratedValue
+    @RevisionNumber
+    private int id;
+
+    @RevisionTimestamp
+    private long timestamp;
 	
 	@Column(name = "user_id")
 	private String userId;

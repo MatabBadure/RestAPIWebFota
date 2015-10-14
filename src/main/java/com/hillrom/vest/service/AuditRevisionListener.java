997caf4db4358e4d130a6e23f9175ebf3e8174a8
Rohan Kushwaha <rohangtr@gmail.com>
2015-10-14 19:41:04 +0530

    Resolved

--- src/main/java/com/hillrom/vest/service/AuditRevisionListener.java
+++ src/main/java/com/hillrom/vest/service/AuditRevisionListener.java
@@ -1,21 +1,27 @@
 package com.hillrom.vest.service;
 
+import java.util.Objects;
+
 import org.hibernate.envers.RevisionListener;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.hillrom.vest.domain.AuditRevisionInfoEntity;
 
-public class AuditRevisionListener implements RevisionListener{
+public class AuditRevisionListener implements RevisionListener {
 
 	@Override
 	public void newRevision(Object revisionEntity) {
 		AuditRevisionInfoEntity auditRevitionInfo = (AuditRevisionInfoEntity) revisionEntity;
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		if (principal instanceof UserDetails) {
-				auditRevitionInfo.setAuditor(((UserDetails)principal).getUsername());
-			} else {
+		if (Objects.nonNull(SecurityContextHolder.getContext().getAuthentication())) {
+			Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			if (principal instanceof UserDetails)
+				auditRevitionInfo.setAuditor(((UserDetails) principal).getUsername());
+			else
 				auditRevitionInfo.setAuditor(principal.toString());
-			}
+		} else
+			//For reset password or activation as no suer is logged in.
+			auditRevitionInfo.setAuditor("AnonymousUser");
+
 	}
 }

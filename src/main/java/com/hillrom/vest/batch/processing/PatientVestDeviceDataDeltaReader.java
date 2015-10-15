69f28917196ad98ad70eecbba394de5cf72a0211
Rohan Kushwaha <rohangtr@gmail.com>
2015-10-15 16:19:19 +0530

    Async working

--- src/main/java/com/hillrom/vest/batch/processing/PatientVestDeviceDataDeltaReader.java
+++ src/main/java/com/hillrom/vest/batch/processing/PatientVestDeviceDataDeltaReader.java
@@ -1,28 +1,44 @@
 package com.hillrom.vest.batch.processing;
 
-import java.lang.management.ManagementFactory;
+import static com.hillrom.vest.security.AuthoritiesConstants.PATIENT;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
-import org.neo4j.cypher.internal.helpers.Converge.iterateUntilConverged;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.annotation.BeforeStep;
+import org.joda.time.LocalDate;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.beans.factory.annotation.Value;
 
-import com.hillrom.vest.domain.PatientInfo;
-import com.hillrom.vest.domain.PatientVestDeviceData;
-import com.hillrom.vest.domain.User;
-import com.hillrom.vest.repository.PatientInfoRepository;
-import com.hillrom.vest.repository.TempRepository;
-import com.hillrom.vest.repository.UserRepository;
-import com.hillrom.vest.web.rest.dto.PatientVestDeviceDataVO;
 import com.hillrom.vest.batch.util.BatchUtil;
+import com.hillrom.vest.domain.PatientInfo;
+import com.hillrom.vest.domain.PatientNoEvent;
+import com.hillrom.vest.domain.PatientVestDeviceData;
+import com.hillrom.vest.domain.PatientVestDeviceRawLog;
+import com.hillrom.vest.domain.TempPatientVestDeviceData;
+import com.hillrom.vest.domain.User;
+import com.hillrom.vest.domain.UserExtension;
+import com.hillrom.vest.domain.UserPatientAssoc;
+import com.hillrom.vest.domain.UserPatientAssocPK;
+import com.hillrom.vest.repository.AuthorityRepository;
+import com.hillrom.vest.repository.PatientInfoRepository;
+import com.hillrom.vest.repository.TempPatientVestDeviceDataRepository;
+import com.hillrom.vest.repository.TempRepository;
+import com.hillrom.vest.repository.UserExtensionRepository;
+import com.hillrom.vest.repository.UserPatientRepository;
+import com.hillrom.vest.repository.UserRepository;
+import com.hillrom.vest.security.AuthoritiesConstants;
+import com.hillrom.vest.service.DeviceLogParser;
+import com.hillrom.vest.service.PatientNoEventService;
+import com.hillrom.vest.util.RelationshipLabelConstants;
+import com.hillrom.vest.web.rest.dto.PatientVestDeviceDataVO;
 
 public class PatientVestDeviceDataDeltaReader implements
 		ItemReader<List<PatientVestDeviceData>> {
@@ -34,12 +50,56 @@ public class PatientVestDeviceDataDeltaReader implements
 	private UserRepository userRepository;
 	
 	@Inject
+	private UserPatientRepository userPatientRepository;
+	
+	@Inject
 	private PatientInfoRepository patientInfoRepository;
+	
+	@Inject
+	private UserExtensionRepository userExtensionRepository;
+	
+	@Inject
+	private  AuthorityRepository authorityRepository;
+	
+	@Inject
+	private DeviceLogParser deviceLogParser;
+	
+	@Inject
+	private TempPatientVestDeviceDataRepository tempPatientVestDeviceDataRepository;
+	
+	@Inject
+	private PatientNoEventService noEventService;	
+	
+	String patientDeviceRawData;
+	
+	@Value("#{jobParameters['rawData']}")
+	public void setRawData(final String rawData) {
+		this.patientDeviceRawData = rawData;
+	    
+	  }
+	
+    private void parseRawData() {
+    
+	 PatientVestDeviceRawLog deviceRawLog = null;
+		List<TempPatientVestDeviceData> tempPatientVestDeviceRecords = null;
+			deviceRawLog = deviceLogParser.parseBase64StringToPatientVestDeviceRawLog(patientDeviceRawData);
+
+			tempPatientVestDeviceRecords = deviceLogParser
+					.parseBase64StringToPatientVestDeviceLogEntryForTemp(deviceRawLog.getDeviceData());
+
+			String deviceSerialNumber = deviceRawLog.getDeviceSerialNumber();
+
+			UserPatientAssoc userPatientAssoc = createPatientUserIfNotExists(deviceRawLog, deviceSerialNumber);
+			assignDefaultValuesToVestDeviceDataTemp(deviceRawLog, tempPatientVestDeviceRecords, userPatientAssoc);
+			tempPatientVestDeviceDataRepository.save(tempPatientVestDeviceRecords);
+	}
 	
 	@Override
 	public List<PatientVestDeviceData> read() throws Exception,
 			UnexpectedInputException, ParseException,
 			NonTransientResourceException {
+		
+		parseRawData();
 		List<PatientVestDeviceDataVO>  deviceDataDelta = tempRepository.getPatientDeviceDataDelta();
 		User patientUser = null;
 		PatientInfo patient = null;
@@ -84,6 +144,89 @@ public class PatientVestDeviceDataDeltaReader implements
 		}
 		BatchUtil.flag = true;
 		return vestDeviceDatas;
+	}
+	
+	private void assignDefaultValuesToVestDeviceDataTemp(PatientVestDeviceRawLog deviceRawLog,
+			List<TempPatientVestDeviceData> patientVestDeviceRecords, UserPatientAssoc userPatientAssoc) {
+		patientVestDeviceRecords.stream().forEach(deviceData -> {
+			deviceData.setHubId(deviceRawLog.getHubId());
+			deviceData.setSerialNumber(deviceRawLog.getDeviceSerialNumber());
+			deviceData.setPatient(userPatientAssoc.getPatient());
+			deviceData.setPatientUser(userPatientAssoc.getUser());
+			deviceData.setBluetoothId(deviceRawLog.getDeviceAddress());
+		});
+	}
+
+	private UserPatientAssoc createPatientUserIfNotExists(PatientVestDeviceRawLog deviceRawLog,
+			String deviceSerialNumber) {
+		Optional<PatientInfo> patientFromDB = patientInfoRepository.findOneBySerialNumber(deviceSerialNumber);
+
+		PatientInfo patientInfo = null;
+
+		if (patientFromDB.isPresent()) {
+			patientInfo = patientFromDB.get();
+			List<UserPatientAssoc> associations = userPatientRepository.findOneByPatientId(patientInfo.getId());
+			List<UserPatientAssoc> userPatientAssociations = associations.stream()
+					.filter(assoc -> RelationshipLabelConstants.SELF.equalsIgnoreCase(assoc.getRelationshipLabel()))
+					.collect(Collectors.toList());
+			return userPatientAssociations.get(0);
+		} else {
+			patientInfo = new PatientInfo();
+			// Assigns the next hillromId for the patient
+			String hillromId = patientInfoRepository.id();
+			patientInfo.setId(hillromId);
+			patientInfo.setHillromId(hillromId);
+			patientInfo.setBluetoothId(deviceRawLog.getDeviceAddress());
+			patientInfo.setHubId(deviceRawLog.getHubId());
+			patientInfo.setSerialNumber(deviceRawLog.getDeviceSerialNumber());
+			String customerName = deviceRawLog.getCustomerName();
+			setNameToPatient(patientInfo, customerName);
+			patientInfo = patientInfoRepository.save(patientInfo);
+
+			UserExtension userExtension = new UserExtension();
+			userExtension.setHillromId(hillromId);
+			userExtension.setActivated(true);
+			userExtension.setDeleted(false);
+			userExtension.setFirstName(patientInfo.getFirstName());
+			userExtension.setLastName(patientInfo.getLastName());
+			userExtension.setMiddleName(patientInfo.getMiddleName());
+			userExtension.getAuthorities().add(authorityRepository.findOne(PATIENT));
+			userExtensionRepository.save(userExtension);
+
+			UserPatientAssoc userPatientAssoc = new UserPatientAssoc(new UserPatientAssocPK(patientInfo, userExtension),
+					AuthoritiesConstants.PATIENT, RelationshipLabelConstants.SELF);
+
+			userPatientRepository.save(userPatientAssoc);
+
+			userExtension.getUserPatientAssoc().add(userPatientAssoc);
+			patientInfo.getUserPatientAssoc().add(userPatientAssoc);
+
+			userExtensionRepository.save(userExtension);
+			patientInfoRepository.save(patientInfo);
+			LocalDate createdOrTransmittedDate = userExtension.getCreatedDate().toLocalDate();
+			noEventService.createIfNotExists(
+					new PatientNoEvent(createdOrTransmittedDate, createdOrTransmittedDate, patientInfo, userExtension));
+			return userPatientAssoc;
+		}
+	}
+	
+	private void setNameToPatient(PatientInfo patientInfo, String customerName) {
+		String names[] = customerName.split(" ");
+		if (names.length == 2) {
+			assignNameToPatient(patientInfo, names[1], names[0], null);
+		}
+		if (names.length == 3) {
+			assignNameToPatient(patientInfo, names[2], names[1], names[0]);
+		}
+		if (names.length == 1) {
+			assignNameToPatient(patientInfo, names[0], null, null);
+		}
+	}
+	
+	private void assignNameToPatient(PatientInfo patientInfo, String firstName, String lastName, String middleName) {
+		patientInfo.setFirstName(firstName);
+		patientInfo.setLastName(lastName);
+		patientInfo.setMiddleName(middleName);
 	}
 
 }

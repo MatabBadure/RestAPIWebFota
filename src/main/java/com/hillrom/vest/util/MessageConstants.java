275da95bc4b9b48c47ce299df661f6d35a2861b2
Rohan Kushwaha <rohangtr@gmail.com>
2015-10-16 14:55:43 +0530

    When no active associated to patient, an exception will be thrown.

--- src/main/java/com/hillrom/vest/util/MessageConstants.java
+++ src/main/java/com/hillrom/vest/util/MessageConstants.java
@@ -45,6 +45,7 @@ public class MessageConstants {
     public static final String HR_264 = "Caregiver User deleted successfully.";
     public static final String HR_265 = "Relationship labels fetched successfully.";
     public static final String HR_266 = "No Caregiver found for patient.";
+    public static final String HR_267 = "No active patient found.";
     
     public static final String HR_271 = "HCP associated with patient successfully.";
     public static final String HR_272 = "HCP is dissociated from Clinic successfully.";

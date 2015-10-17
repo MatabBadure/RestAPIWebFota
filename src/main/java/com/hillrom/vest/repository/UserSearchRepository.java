1ecb30ae57e6efd9ba22ff157cd1d49dd17226b9
Rohan Kushwaha <rohangtr@gmail.com>
2015-10-17 16:16:59 +0530

    IsActiveData value corrected

--- src/main/java/com/hillrom/vest/repository/UserSearchRepository.java
+++ src/main/java/com/hillrom/vest/repository/UserSearchRepository.java
@@ -747,7 +747,7 @@ public class UserSearchRepository {
 					}
 
 					PatientUserVO patientUserVO = new PatientUserVO(id, email, firstName,
-							lastName, IsDeletedInClinic, zipcode, address, city, dobLocalDate,
+							lastName, IsDeletedInClinic?false:true, zipcode, address, city, dobLocalDate,
 							gender, title, hillromId,createdAtDatetime,isActivated,state,
 							Objects.nonNull(adherence) ? adherence : 0,localLastTransmissionDate);
 					//mrnId,hcpNamesCSV,clinicNamesCSV
@@ -1044,7 +1044,7 @@ public class UserSearchRepository {
 					}
 
 					PatientUserVO patientUserVO = new PatientUserVO(id, email, firstName,
-							lastName, isActivatedInClinic, zipcode, address, city, dobLocalDate,
+							lastName, isActivatedInClinic?false:true, zipcode, address, city, dobLocalDate,
 							gender, title, hillromId,createdAtDatetime,isActivated,state,
 							Objects.nonNull(adherence) ? adherence : 0,localLastTransmissionDate);
 					//mrnId,hcpNamesCSV,clinicNamesCSV

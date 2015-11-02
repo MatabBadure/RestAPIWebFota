49ad5ced07293b2edbf63c97510cfe9ca434b679
Rohan Kushwaha <rohangtr@gmail.com>
2015-11-02 17:03:00 +0530

    Job running time changes to every hour

--- src/main/java/com/hillrom/vest/service/MailService.java
+++ src/main/java/com/hillrom/vest/service/MailService.java
@@ -206,7 +206,7 @@ public class MailService {
         sendEmail(new String[]{user.getEmail()}, subject, content, false, true);
     }
     
-    @Scheduled(cron="0 0/1 * * * *")
+    @Scheduled(cron="0 0 * * * *")
 	@Async
 	public void activationReminderEmail(){
 		DateTime currectTime =  new DateTime();

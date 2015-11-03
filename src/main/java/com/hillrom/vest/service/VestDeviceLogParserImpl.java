c7bfb4574d45e0fa4be9915d8abf6aa3ba5e64ac
gopakumark <gopakumar.kondayin@razorfish.com>
2015-11-03 18:08:17 +0530

    UnCommented code to accept base64 String

--- src/main/java/com/hillrom/vest/service/VestDeviceLogParserImpl.java
+++ src/main/java/com/hillrom/vest/service/VestDeviceLogParserImpl.java
@@ -93,10 +93,10 @@ public class VestDeviceLogParserImpl implements DeviceLogParser {
 
 	@Override
 	public List<PatientVestDeviceData> parseBase64StringToPatientVestDeviceLogEntry(
-			String base16String) {
+			String base64String) {
 
 		List<PatientVestDeviceData> patientVestDeviceRecords = new LinkedList<>();
-		//String base16String = ParserUtil.convertToBase16String(base64String);
+		String base16String = ParserUtil.convertToBase16String(base64String);
 		if (base16String.length() <= 16)
 			return patientVestDeviceRecords;
 

4ad9971ce602de7e016cc5e29f8e2e1b3a701e54
Rohan Kushwaha <rohangtr@gmail.com>
2015-11-18 18:50:28 +0530

    Corrected dissociate API.

--- src/main/java/com/hillrom/vest/repository/EntityUserRepository.java
+++ src/main/java/com/hillrom/vest/repository/EntityUserRepository.java
@@ -11,6 +11,9 @@ import com.hillrom.vest.domain.EntityUserAssocPK;
 public interface EntityUserRepository extends JpaRepository<EntityUserAssoc, EntityUserAssocPK> {
 
 	@Query("from EntityUserAssoc eua where eua.entityUserAssocPK.clinic.id = ?1 and eua.userRole = ?2")
-	List<EntityUserAssoc> findByClinicIdAndUserRole(String clinic, String userRole);
+	List<EntityUserAssoc> findByClinicIdAndUserRole(String clinicId, String userRole);
+	
+	@Query("from EntityUserAssoc eua where eua.entityUserAssocPK.user.id = ?1 and eua.entityUserAssocPK.clinic.id=?2 and eua.userRole = ?3")
+	EntityUserAssoc findByUserIdAndClinicIdAndUserRole(Long id, String clinicId, String userRole);
 
 }

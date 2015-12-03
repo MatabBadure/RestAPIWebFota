de2a89b0f83b5b13fd6252e3a9e2f25c97ecec63
gopakumark <gopakumar.kondayin@razorfish.com>
2015-12-03 12:24:40 +0530

    In Progress

--- src/main/java/com/hillrom/vest/web/rest/GraphResource.java
+++ src/main/java/com/hillrom/vest/web/rest/GraphResource.java
@@ -1,29 +1,24 @@
 package com.hillrom.vest.web.rest;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Objects;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StringReader;
 
-import javax.inject.Inject;
-import javax.validation.Valid;
-
-import net.minidev.json.JSONObject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hillrom.vest.exceptionhandler.HillromException;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 
 /**
@@ -39,25 +34,51 @@ public class GraphResource {
 
     /**
      * POST  /securityQuestions -> Post base64 Graph PDF string.
+     * @throws IOException 
      */
     @RequestMapping(value = "/graph/pdfDownload",
-            method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    
-    public ResponseEntity<Object> downloadPDF(@RequestBody Map<String, String> base64EncodedString) {
-        log.debug("REST request to return base64 Encoded String", base64EncodedString);
-        JSONObject jsonObject = new JSONObject();
-        try{
-	        if (!Objects.nonNull(base64EncodedString)) {
-	            return ResponseEntity.badRequest().body(jsonObject);
-	        }
-	        	
-	        return new ResponseEntity<Object>(base64EncodedString,HttpStatus.OK);
-	        	        
-		} catch (Exception e) {
-			jsonObject.put("ERROR",e.getMessage()); 
-			return ResponseEntity.badRequest().body(jsonObject);
-		}
+            method = RequestMethod.GET,
+            produces = "application/pdf")
+    public ResponseEntity<?> downloadPDF(HttpServletResponse response) throws IOException {
+    	Document document = new Document();
+        try
+        {
+           PdfWriter.getInstance(document, response.getOutputStream());
+           document.open();
+           document.add(new Paragraph("<h1>A Hello World PDF document.</h1>"));
+           document.add(new Paragraph("<h1>Next line.</h1>"));
+           document.close();
+           return null;
+        } catch (DocumentException e)
+        {
+           e.printStackTrace();
+        } catch (FileNotFoundException e)
+        {
+           e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
+    public void emitJPG( HttpServletRequest request,
+    	    HttpServletResponse response, String svgString )
+    	{
+    	    response.setContentType("image/jpeg");
+
+    	    JPEGTranscoder t = new JPEGTranscoder();
+    	    t.addTranscodingHint(JPEGTranscoder.KEY_QUALITY,
+    	                         new Float(.8));
+
+    	    TranscoderInput input =
+    	        new TranscoderInput( new StringReader(svgString) );
+    	    try {
+    	        TranscoderOutput output =
+    	            new TranscoderOutput(response.getOutputStream());
+    	        t.transcode(input, output);
+    	        response.getOutputStream().close();
+    	    }
+    	    catch (Exception e)
+    	    {
+    	        e.printStackTrace();
+    	    }
+    	}
 }

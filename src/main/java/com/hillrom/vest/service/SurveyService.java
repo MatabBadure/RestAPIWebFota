800f949d6ce24d6df9edadb6cecad8add2e9dc53
Rohan Kushwaha <rohangtr@gmail.com>
2016-03-01 19:27:42 +0530

    Completed

--- src/main/java/com/hillrom/vest/service/SurveyService.java
+++ src/main/java/com/hillrom/vest/service/SurveyService.java
@@ -9,8 +9,6 @@ import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 
-import net.minidev.json.JSONObject;
-
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 import org.springframework.stereotype.Service;
@@ -29,10 +27,13 @@ import com.hillrom.vest.repository.SurveyRepository;
 import com.hillrom.vest.repository.UserRepository;
 import com.hillrom.vest.repository.UserSurveyAnswerRepository;
 import com.hillrom.vest.service.util.DateUtil;
+import com.hillrom.vest.service.util.RandomUtil;
 import com.hillrom.vest.util.ExceptionConstants;
 import com.hillrom.vest.web.rest.dto.SurveyQuestionVO;
 import com.hillrom.vest.web.rest.dto.SurveyVO;
 import com.hillrom.vest.web.rest.dto.UserSurveyAnswerDTO;
+
+import net.minidev.json.JSONObject;
 
 @Service
 @Transactional
@@ -51,18 +52,13 @@ public class SurveyService {
 	private UserRepository userRepository;
 
 	@Inject
+	private MailService mailService;
+
+	@Inject
 	private UserService userService;
-	
+
 	@Inject
 	private PatientNoEventService noEventService;
-
-	public static final Long FIVE_DAY_SURVEY_ID = 1L;
-	public static final Long THIRTY_DAY_SURVEY_ID = 2L;
-	public static final Long NIGHTY_DAY_SURVEY_ID = 3L;
-
-	public static final Integer FIVE_DAYS = 5;
-	public static final Integer THIRTY_DAYS = 30;
-	public static final Integer NINTY_DAYS = 90;
 
 	public List<Survey> getAllSurveys() {
 		return surveyRepository.findAll();
@@ -107,7 +103,11 @@ public class SurveyService {
 		return questionVO;
 	}
 
-	public void createSurveyAnswer(UserSurveyAnswerDTO userSurveyAnswerDTO) throws HillromException {
+	public void createSurveyAnswer(UserSurveyAnswerDTO userSurveyAnswerDTO, String baseUrl) throws HillromException {
+		// Take survey only if you are eligible to take
+		if (getDueSurveyByUserId(userSurveyAnswerDTO.getUserId()).getId() != userSurveyAnswerDTO.getSurveyId())
+			throw new HillromException(ExceptionConstants.HR_806);
+
 		if (Objects.isNull(userSurveyAnswerDTO.getUserSurveyAnswer())
 				|| userSurveyAnswerDTO.getUserSurveyAnswer().isEmpty())
 			throw new HillromException(ExceptionConstants.HR_802);
@@ -129,6 +129,8 @@ public class SurveyService {
 		}
 
 		userSurveyAnswerRepository.save(userSurveyAnswers);
+		// Email
+		mailService.sendSurveyEmailReport(userSurveyAnswerDTO, baseUrl);
 	}
 
 	public UserSurveyAnswerDTO getSurveyAnswerById(Long id) throws HillromException {
@@ -143,8 +145,8 @@ public class SurveyService {
 		return userSurveyAnswerDTO;
 	}
 
-	public Survey getDueSurveyByUserId(Long id) throws HillromException {
-		User user = userRepository.findOne(id);
+	public Survey getDueSurveyByUserId(Long userId) throws HillromException {
+		User user = userRepository.findOne(userId);
 		if (Objects.isNull(user))
 			throw new HillromException(ExceptionConstants.HR_512);
 
@@ -152,45 +154,45 @@ public class SurveyService {
 			throw new HillromException(ExceptionConstants.HR_523);
 
 		// Checking whether first transmission was done
-		PatientNoEvent noEvent = noEventService.findByPatientUserId(id);
-		if( Objects.isNull(noEvent) || Objects.isNull(noEvent.getFirstTransmissionDate()))
+		PatientNoEvent noEvent = noEventService.findByPatientUserId(userId);
+		if (Objects.isNull(noEvent) || Objects.isNull(noEvent.getFirstTransmissionDate()))
 			throw new HillromException(ExceptionConstants.HR_804);
-		
+
 		LocalDate firstTransmissionDate = noEvent.getFirstTransmissionDate();
-		
-		int daysDifference = DateUtil.getDaysCountBetweenLocalDates(firstTransmissionDate, LocalDate.now()); 
-		if ((daysDifference >= FIVE_DAYS && daysDifference < THIRTY_DAYS)
-				&& userSurveyAnswerRepository.findCountByUserIdAndSurveyId(id, FIVE_DAY_SURVEY_ID) < 1) {
-			return surveyRepository.findOne(FIVE_DAY_SURVEY_ID);
-		} else if (daysDifference >= THIRTY_DAYS
-				&& daysDifference < NINTY_DAYS
-				&& userSurveyAnswerRepository.findCountByUserIdAndSurveyId(id, THIRTY_DAY_SURVEY_ID) < 1) {
-			return surveyRepository.findOne(THIRTY_DAY_SURVEY_ID);
-		} else if (daysDifference >= NINTY_DAYS
-				&& userSurveyAnswerRepository.findCountByUserIdAndSurveyId(id, NIGHTY_DAY_SURVEY_ID) < 1) {
-			return surveyRepository.findOne(NIGHTY_DAY_SURVEY_ID);
+
+		int daysDifference = DateUtil.getDaysCountBetweenLocalDates(firstTransmissionDate, LocalDate.now());
+		if ((daysDifference >= RandomUtil.FIVE_DAYS && daysDifference < RandomUtil.THIRTY_DAYS)
+				&& userSurveyAnswerRepository.findCountByUserIdAndSurveyId(userId, RandomUtil.FIVE_DAY_SURVEY_ID) < 1) {
+			return surveyRepository.findOne(RandomUtil.FIVE_DAY_SURVEY_ID);
+		} else if (daysDifference >= RandomUtil.THIRTY_DAYS && daysDifference < RandomUtil.NINTY_DAYS
+				&& userSurveyAnswerRepository.findCountByUserIdAndSurveyId(userId,
+						RandomUtil.THIRTY_DAY_SURVEY_ID) < 1) {
+			return surveyRepository.findOne(RandomUtil.THIRTY_DAY_SURVEY_ID);
+		} else if (daysDifference >= RandomUtil.NINTY_DAYS && userSurveyAnswerRepository
+				.findCountByUserIdAndSurveyId(userId, RandomUtil.NIGHTY_DAY_SURVEY_ID) < 1) {
+			return surveyRepository.findOne(RandomUtil.NIGHTY_DAY_SURVEY_ID);
 		} else
 			throw new HillromException(ExceptionConstants.HR_804);
 	}
 
 	public JSONObject getGridView(Long surveyId, LocalDate fromDate, LocalDate toDate) throws HillromException {
 		JSONObject responseJSON = new JSONObject();
-		if (FIVE_DAY_SURVEY_ID.equals(surveyId)) {
+		if (RandomUtil.FIVE_DAY_SURVEY_ID.equals(surveyId)) {
 			responseJSON.put("count", userSurveyAnswerRepository.findSurveyCountByDateRange(surveyId,
 					fromDate.toDateTime(LocalTime.MIDNIGHT), toDate.plusDays(1).toDateTime(LocalTime.MIDNIGHT)));
 			responseJSON.put("surveyGridView",
 					userSurveyAnswerRepository.fiveDaySurveyReport(fromDate.toString(), toDate.toString()));
 			return responseJSON;
-		} else if (THIRTY_DAY_SURVEY_ID.equals(surveyId)) {
+		} else if (RandomUtil.THIRTY_DAY_SURVEY_ID.equals(surveyId)) {
 			responseJSON.put("count", userSurveyAnswerRepository.findSurveyCountByDateRange(surveyId,
 					fromDate.toDateTime(LocalTime.MIDNIGHT), toDate.plusDays(1).toDateTime(LocalTime.MIDNIGHT)));
 			responseJSON.put("surveyGridView",
 					userSurveyAnswerRepository.thirtyDaySurveyReport(fromDate.toString(), toDate.toString()));
 			return responseJSON;
-		} else if (NIGHTY_DAY_SURVEY_ID.equals(surveyId)) {
+		} else if (RandomUtil.NIGHTY_DAY_SURVEY_ID.equals(surveyId)) {
 			responseJSON.put("count", userSurveyAnswerRepository.findSurveyCountByDateRange(surveyId,
 					fromDate.toDateTime(LocalTime.MIDNIGHT), toDate.plusDays(1).toDateTime(LocalTime.MIDNIGHT)));
-			responseJSON.put("surveyGridView",getNintyDaySurveyResponse(fromDate.toString(), toDate.toString()));
+			responseJSON.put("surveyGridView", getNintyDaySurveyResponse(fromDate.toString(), toDate.toString()));
 			return responseJSON;
 		} else
 			throw new HillromException(ExceptionConstants.HR_801);
@@ -198,7 +200,7 @@ public class SurveyService {
 
 	private List<NintyDaySurveyReportVO> getNintyDaySurveyResponse(String fromDateTime, String toDateTime) {
 		Map<Long, List<NintyDaysResultSetVO>> nintyDaysRSGroupedByUserID = (Map) userSurveyAnswerRepository
-				.nintyDaySurveyReport(fromDateTime,toDateTime).stream()
+				.nintyDaySurveyReport(fromDateTime, toDateTime).stream()
 				.collect(Collectors.groupingBy(NintyDaysResultSetVO::getUserId));
 
 		List<NintyDaySurveyReportVO> nintyDaySurveyReportVOs = new LinkedList<>();
@@ -217,6 +219,5 @@ public class SurveyService {
 			nintyDaySurveyReportVOs.add(nintyDaySurveyReportVO);
 		}
 		return nintyDaySurveyReportVOs;
-
 	}
 }

3cc24c3d4a4b5c6ddf1d27d1e4400f112d61cc22
ragnaray <raghu.narayanan@neevtech.com>
2016-03-23 15:21:30 +0530

    defect for ArrayIndexOutOfBoundsException

--- src/main/java/com/hillrom/vest/service/SurveyService.java
+++ src/main/java/com/hillrom/vest/service/SurveyService.java
@@ -258,10 +258,14 @@ public class SurveyService {
 			if(Constants.YES.equals(surveyAnswerResultSetVO.get(3).getAnswerValue()))
 				continue;
 			fiveDayViewVO = new FiveDayViewVO(
-					surveyAnswerResultSetVO.get(0).getAnswerValue(),
-					surveyAnswerResultSetVO.get(1).getAnswerValue(),
-					surveyAnswerResultSetVO.get(2).getAnswerValue(),
-					surveyAnswerResultSetVO.get(3).getAnswerValue2());
+					Objects.nonNull(surveyAnswerResultSetVO.get(0).getAnswerValue())?
+					surveyAnswerResultSetVO.get(0).getAnswerValue():"",
+					Objects.nonNull(surveyAnswerResultSetVO.get(1).getAnswerValue())?
+					surveyAnswerResultSetVO.get(1).getAnswerValue():"",
+					Objects.nonNull(surveyAnswerResultSetVO.get(2).getAnswerValue())?
+					surveyAnswerResultSetVO.get(2).getAnswerValue():"",
+					Objects.nonNull(surveyAnswerResultSetVO.get(3).getAnswerValue2())?
+					surveyAnswerResultSetVO.get(3).getAnswerValue2():"");
 			fiveDayViewVOs.add(fiveDayViewVO);
 		}
 		return fiveDayViewVOs;

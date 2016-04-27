fde46380b53f082f1a2968b59b056e12aad323a5
gopakumark <gopakumar.kondayin@razorfish.com>
2016-04-27 19:14:46 +0530

    Added Api for creating test result by HCP and Clinic Admin

--- src/main/java/com/hillrom/vest/repository/EntityUserRepository.java
+++ src/main/java/com/hillrom/vest/repository/EntityUserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.hillrom.vest.domain.EntityUserAssoc;
 import com.hillrom.vest.domain.EntityUserAssocPK;
@@ -18,4 +19,8 @@ public interface EntityUserRepository extends JpaRepository<EntityUserAssoc, Ent
 	
 	@Query("from EntityUserAssoc eua where eua.entityUserAssocPK.user.id = ?1 and eua.userRole = ?2")
 	List<EntityUserAssoc> findByUserIdAndUserRole(Long Id, String userRole);
+	
+	@Query(nativeQuery=true,value="SELECT user_id from ENTITY_USER_ASSOC eua join CLINIC_PATIENT_ASSOC cpa "
+			+ "on eua.user_id = :userId and cpa.patient_id = :patientId and cpa.is_active = :isActive and eua.entity_id = cpa.clinic_id")
+	Long returnUserIdIfAssociationExists(@Param("userId")Long userId,@Param("patientId")String patientId,@Param("isActive")boolean isActive);
 }

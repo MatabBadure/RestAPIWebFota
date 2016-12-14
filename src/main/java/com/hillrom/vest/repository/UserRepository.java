38337f991181a84bc9944678fe9c3495453b7b54
Soujanya K <Soujanya.K@Lnttechservices.com>
2016-12-14 13:00:33 +0530

    Hill-1852_CronJob_Changes

--- src/main/java/com/hillrom/vest/repository/UserRepository.java
+++ src/main/java/com/hillrom/vest/repository/UserRepository.java
@@ -9,6 +9,10 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.hillrom.vest.domain.User;
 
+//Hill-1852
+import java.time.LocalDate;
+//Hill-1852
+
 /**
  * Spring Data JPA repository for the User entity.
  */
@@ -35,4 +39,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("from User user where activated = false and activationLinkSentDate between ?1 and ?2")
     List<User> findAllByActivatedIsFalseAndActivationLinkSentDateBetweeen(DateTime dateTime1, DateTime dateTime2);
 
+    
+    //Hill-1852
+    @Query(" SELECT pi.id, pi.firstName, pi.lastName, pi.email, pi.dob "
+            + " ,uupa.id ,uupa.email, uupa.firstName ,uupa.lastName, uupa.activationKey "
+            + " FROM PatientInfo pi "
+            + " left join pi.userPatientAssoc upa "
+            + " left join upa.userPatientAssocPK.user uupa  "
+            + " where upa.relationshipLabel = 'Caregiver' and upa.userRole = 'CARE_GIVER' and "
+    		+ " MONTH(pi.dob) = MONTH(?1) and DAY(pi.dob) = DAY(?1) and YEAR(pi.dob) + 18 = YEAR(?1) ")
+       List<Object[]> findUserDOB(LocalDate minDate);
+     //Hill-1852
+       
 }

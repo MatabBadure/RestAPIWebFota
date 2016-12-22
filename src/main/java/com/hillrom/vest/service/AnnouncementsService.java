320fdf4cb30141bb68db1a8f84b0bb20537ffaec
Soujanya K <Soujanya.K@Lnttechservices.com>
2016-12-22 13:23:07 +0530

    hill-1854_Ann_Review_2

--- src/main/java/com/hillrom/vest/service/AnnouncementsService.java
+++ src/main/java/com/hillrom/vest/service/AnnouncementsService.java
@@ -1,22 +1,43 @@
 package com.hillrom.vest.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hillrom.vest.domain.Announcements;
 import com.hillrom.vest.exceptionhandler.HillromException;
 import com.hillrom.vest.repository.AnnouncementsRepository;
+import com.hillrom.vest.security.AuthoritiesConstants;
 import com.hillrom.vest.service.util.DateUtil;
 import com.hillrom.vest.web.rest.dto.AnnouncementsDTO;
+import com.hillrom.vest.web.rest.dto.ClinicVO;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Paths;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 @Service
 @Transactional
@@ -26,6 +47,14 @@ public class AnnouncementsService {
 	
 	@Inject
 	private AnnouncementsRepository announcementsRepository;
+	
+	@Inject
+	private ClinicService clinicService;
+	
+	@Inject
+	private HCPClinicService hcpClinicService; 
+	
+	
 
 	/**
 	 * 
@@ -48,6 +77,7 @@ public class AnnouncementsService {
 		announcement.setPatientType(announcementsDTO.getPatientType());
 		announcement.setDeleted(false);
 		announcementsRepository.save(announcement);
+		
         log.debug("Created New Announcement: {}", announcement);
         return announcement;
 	}
@@ -57,24 +87,64 @@ public class AnnouncementsService {
 	 * @return
 	 * @throws HillromException
 	 */
- public Page<Announcements> findAnnouncementData(Pageable pageable) throws HillromException{
+	public Page<Announcements> findAllAnnouncements(Pageable pageable) throws HillromException{
 		
 		Page<Announcements> announcements = announcementsRepository.findAnnouncements(false,pageable);
 		return announcements;
 	}
  
-/**
- * 
- * @param id
- * @return
- * @throws HillromException
- */
- public Announcements findAnnouncementById(Long id) throws HillromException{
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * @throws HillromException
+	 */
+	public Announcements findAnnouncementById(Long id) throws HillromException{
 		
 		Announcements announcements = announcementsRepository.findOneById(id,false);
 		return announcements;
 	}
+
+
+
+
  
+	public Page<Announcements> findVisibleAnnouncementsById(String userType, Long userId,String patientId,Pageable pageable) throws HillromException{
+			
+	 		Page<Announcements> announcementList = null; // new ArrayList<Announcements>();
+	 		List<String> clinicList = null;
+	 		
+	 		if(userType.equalsIgnoreCase(AuthoritiesConstants.CLINIC_ADMIN)){
+	 			Set<ClinicVO> clinics = clinicService.getAssociatedClinicsForClinicAdmin(userId);
+	 			for(ClinicVO tclinic : clinics){
+	 				clinicList.add(tclinic.getId());
+	 			}
+	 		}
+	 		
+	 		if(userType.equalsIgnoreCase(AuthoritiesConstants.HCP)){
+	 			List<ClinicVO> clinics = hcpClinicService.getAssociatedClinicsForHCP(userId);
+	 			for(ClinicVO tclinic : clinics){
+	 				clinicList.add(tclinic.getId());
+	 			}
+	 		}
+	 		
+	 		
+	 		// Check for the clinic flag to differentiate between whether the clinic id is passed or patient id is passed
+			if(Objects.nonNull(clinicList)){
+				announcementList = announcementsRepository.findAnnouncementsByClinicId(clinicList, false,pageable);
+			}
+			
+			if(Objects.nonNull(patientId)){
+				announcementList = announcementsRepository.findAnnouncementsByPatientId(patientId, false,pageable);
+			}
+			
+	        
+	        return announcementList;
+	}
+	 
+	 
+	 
+	 
  /**
   * 
   * @param announcementsDTO
@@ -112,12 +182,14 @@ public class AnnouncementsService {
     	  Announcements announcement = announcementsRepository.findOne(id);
     	  if(Objects.nonNull(announcement))
     	  {
-    			  announcement.setDeleted(true);
+    			  announcement.setDeleted(false);
     			  announcement.setModifiedDate(DateUtil.getCurrentDateAndTime());
     			  announcementsRepository.save(announcement);
     	          log.debug("updated Announcement Details: {}", announcement);
     	  }
     	  return announcement;	
       }
+ 
+
 	
 }
--- src/main/java/com/hillrom/vest/web/rest/AnnouncementsResource.java
+++ src/main/java/com/hillrom/vest/web/rest/AnnouncementsResource.java
@@ -1,19 +1,32 @@
 package com.hillrom.vest.web.rest;
 
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+
 import net.minidev.json.JSONObject;
 
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+
+import com.hillrom.vest.config.Constants;
 import com.hillrom.vest.domain.Announcements;
+import com.hillrom.vest.security.AuthoritiesConstants;
 import com.hillrom.vest.service.AnnouncementsService;
 import com.hillrom.vest.web.rest.dto.AnnouncementsDTO;
 import com.hillrom.vest.web.rest.util.PaginationUtil;
@@ -23,7 +36,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 
 
 @RestController
@@ -33,6 +49,7 @@ public class AnnouncementsResource {
 	@Inject
 	private AnnouncementsService announcementsService;
 	
+		
 	/**
      * POST  /Announcement -> Create New Announcements
      */
@@ -56,7 +73,6 @@ public class AnnouncementsResource {
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 
-	
 	/**
      * GET  /Announcement -> get All Announcements
      */
@@ -64,7 +80,9 @@ public class AnnouncementsResource {
 	public ResponseEntity<?> getAnnouncements(@RequestParam(value = "page" , required = false) Integer offset,
             @RequestParam(value = "per_page", required = false) Integer limit,
             @RequestParam(value = "sort_by", required = false) String sortBy,
-            @RequestParam(value = "asc",required = false) Boolean isAscending){
+            @RequestParam(value = "asc",required = false) Boolean isAscending,
+            @RequestParam(value = "userType", required = false) String userType,
+            @RequestParam(value = "userTypeId", required = false) Long userId){
 
 	   	 Map<String,Boolean> sortOrder = new HashMap<>();
 	   	 if(sortBy != null  && !sortBy.equals("")) {
@@ -75,7 +93,17 @@ public class AnnouncementsResource {
 		JSONObject jsonObject = new JSONObject();
 		
 		try{
-			 Page<Announcements> announcementsList = announcementsService.findAnnouncementData(PaginationUtil.generatePageRequest(offset, limit, sortOrder));
+			Page<Announcements> announcementsList = null;
+			if(userType.equalsIgnoreCase(AuthoritiesConstants.ADMIN)){
+				announcementsList = announcementsService.findAllAnnouncements(PaginationUtil.generatePageRequest(offset, limit, sortOrder));
+			}
+			if(userType.equalsIgnoreCase(AuthoritiesConstants.CLINIC_ADMIN) || userType.equalsIgnoreCase(AuthoritiesConstants.HCP)){
+				announcementsList = announcementsService.findVisibleAnnouncementsById(userType,userId,null,PaginationUtil.generatePageRequest(offset, limit, sortOrder));	
+			}
+			if(userType.equalsIgnoreCase(AuthoritiesConstants.PATIENT)){
+				announcementsList = announcementsService.findVisibleAnnouncementsById(userType,null,userId.toString(),PaginationUtil.generatePageRequest(offset, limit, sortOrder));	
+			}
+			
 			 jsonObject.put("Announcement_List", announcementsList);
 			 if(Objects.nonNull(announcementsList)){
 				jsonObject.put("announcementMsg", "All Announcements retrieved successfully");
@@ -88,7 +116,6 @@ public class AnnouncementsResource {
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 
-	
 	
 	/**
      * GET  /announcement -> get announcement based on id
@@ -111,6 +138,9 @@ public class AnnouncementsResource {
 		}		
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
+	
+
+
 	
 	/**
      * POST  /announcement -> update announcement based on id
@@ -156,5 +186,85 @@ public class AnnouncementsResource {
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
+	
+	/**
+	   * POST /uploadFile -> receive and locally save a file.
+	   * 
+	   * @param uploadfile The uploaded file as Multipart file parameter in the 
+	   * HTTP request. The RequestParam name must be the same of the attribute 
+	   * "name" in the input tag with type file.
+	   * 
+	   * @return An http OK status in case of success, an http 4xx status in case 
+	   * of errors.
+	   * 
+	   * While calling from pastman pass x-auth-token and name = uploadfile . Body should be form-data , uploadfile and ChooseFile
+	   */
+	  @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+	  @ResponseBody
+	  public ResponseEntity<?> uploadFile(
+	      @RequestParam("uploadfile") MultipartFile uploadfile) {
+	    
+		  String filename = null;
+		  String directory = null;
+		  String filepath = null;
+		  
+		  JSONObject jsonObject = new JSONObject();
+		  
+	    try {
+	    		// Get the filename and build the local file path
+	    		filename = uploadfile.getOriginalFilename();
+	    		directory = Constants.ANNOUNCEMENT_FILE_PATH;
+	    		filepath = Paths.get(directory, filename).toString();
+	      
+	    		// Save the file locally
+	    		BufferedOutputStream stream =
+	    				new BufferedOutputStream(new FileOutputStream(new File(filepath)));
+	    		stream.write(uploadfile.getBytes());
+	    		stream.close();
+	    		
+	    		 jsonObject.put("filepath", filepath);
+	    		 
+	    		 return new ResponseEntity<>(jsonObject, HttpStatus.CREATED);
+	    		 
+	    }
+	    catch (Exception ex) {
+	    	jsonObject.put("ERROR", ex.getMessage());
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	    }
+	  
+	  } 
+	  
+	  /**
+	   * 
+	   * @param fileName
+	   * @param response
+	   */
+	  @RequestMapping(value = "/files/{file_name}", method = RequestMethod.GET)
+	  public ResponseEntity<?> getFile(
+	      @PathVariable("file_name") String fileName, 
+	      HttpServletResponse response) {
+		  
+		  		JSONObject jsonObject = new JSONObject();
+	      try {
+	    	  // get your file as InputStream
+	    	  
+	    	  File initialFile = new File(Constants.ANNOUNCEMENT_FILE_PATH + fileName + ".pdf");
+	    	  InputStream is = new FileInputStream(initialFile);  
+	    	  response.addHeader("Content-disposition", "inline;filename="+Constants.ANNOUNCEMENT_FILE_PATH + fileName);
+	    	  response.setContentType("application/pdf");
+	        // copy it to response's OutputStream
+	        org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+	        response.flushBuffer();
+	        jsonObject.put("response", response);
+	        return new ResponseEntity<>(jsonObject, HttpStatus.CREATED);
+	        
+	      } catch (IOException ex) {
+	    		jsonObject.put("ERROR", ex.getMessage());
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	      }
 
+	  }
+	  
+	
+	
 }

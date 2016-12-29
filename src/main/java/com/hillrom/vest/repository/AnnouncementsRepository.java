4b8cd3a859eb12fd54ef628259ea80aa92328725
Soujanya K <Soujanya.K@Lnttechservices.com>
2016-12-29 18:57:20 +0530

    hill

--- src/main/java/com/hillrom/vest/repository/AnnouncementsRepository.java
+++ src/main/java/com/hillrom/vest/repository/AnnouncementsRepository.java
@@ -24,22 +24,12 @@ public interface AnnouncementsRepository extends JpaRepository<Announcements, Lo
 	    Page<Announcements> findAnnouncements(boolean isDeleted,Pageable pageable);
 	    
 	    // get Clinic iformations based on Id Deatils
-	    @Query(" from Announcements announcement, Clinic clinic where (announcement.sendTo = 'All' or announcement.sendTo = 'Clinic') "
+	    @Query("select distinct announcement.id, announcement.name, announcement.subject, announcement.startDate, announcement.endDate, announcement.createdDate, "
+	    	   + " announcement.modifiedDate, announcement.sendTo,announcement.clinicType,announcement.pdfFilePath, announcement.patientType, announcement.isDeleted "
+	    	   + " from Announcements announcement, Clinic clinic where (announcement.sendTo = 'All' or announcement.sendTo = 'Clinic') "
 	    	   + " and (announcement.clinicType = 'All' or announcement.clinicType = clinic.speciality) and clinic.id in ?1  and announcement.isDeleted = ?2 "
 	    	   + " and CURRENT_DATE() between announcement.startDate and announcement.endDate ")
 	    Page<Announcements> findAnnouncementsByClinicId(List<String> clinicIds, boolean isDeleted,Pageable pageable);
 	
-	    /*
-	    @Query(value=" SELECT * from  ANNOUNCEMENTS ac, PATIENT_INFO pi "
-				+ " where (ac.send_to = 'All' or ac.send_to = 'Patient') and (ac.patient_type = 'All' or pi.primary_diagnosis = ac.patient_type or "
-				+ " IF(TIMESTAMPDIFF(YEAR, pi.dob, CURDATE()) > 18, 'Adult', 'Peds') = ac.patient_type )  "
-				+ " and pi.id =:patientId and ac.is_deleted =:isDeleted and  ac.start_date <= now() and (ac.end_date >= now())", nativeQuery=true)
-	    List<Announcements> findAnnouncementsByPatientId(@Param("patientId")String patientId,@Param("isDeleted")boolean isDeleted,Pageable pageable);
-	    */
-	     /* @Query(" from Announcements announcement, PatientInfo patientInfo where (announcement.sendTo = 'All' or announcement.sendTo = 'Patient') "
- 	   + " and (announcement.patientType = 'All' or announcement.patientType = patientInfo.primaryDiagnosis or "
-	   + " IF (DATEDIFF(YEAR, pi.dob,CURRENT_DATE()) > 18, 'Adult', 'Peds') = announcement.patientType  )"
- 	   + " and patientInfo.id = ?1  and announcement.isDeleted = ?2  ")
-	      List<Announcements> findOneByPatientId(String patientId, boolean isDeleted);*/
-	    
+	   
 }

86bbd65aba8b507c059688534f34e57557af8d1a
Vijayanand Jayapal <Vijayanand.Jayapal@Lnttechservices.com>
2017-05-24 11:11:24 +0530

    modified files for cough pause reduction in duration

--- src/main/java/com/hillrom/vest/service/util/monarch/PatientVestDeviceTherapyUtilMonarch.java
+++ src/main/java/com/hillrom/vest/service/util/monarch/PatientVestDeviceTherapyUtilMonarch.java
@@ -201,7 +201,7 @@ public class PatientVestDeviceTherapyUtilMonarch {
 		
 		metricsMapMonarch.put(FREQUENCY, Math.round(frequency));
 		metricsMapMonarch.put(INTENSITY, Math.round(intensity));
-		metricsMapMonarch.put(DURATION, durationOfSessionMonarch-totalCoughPauseDuration);
+		metricsMapMonarch.put(DURATION, durationOfSessionMonarch-(totalCoughPauseDuration/60));
 		metricsMapMonarch.put(NORMAL_COUGH_PAUSES, normalCoughPauses);
 		metricsMapMonarch.put(PROGRAMMED_COUGH_PAUSES, (coughPauses == 0 ? progCoughPauses : coughPauses));
 		metricsMapMonarch.put(CAUGH_PAUSE_DURATION, totalCoughPauseDuration);

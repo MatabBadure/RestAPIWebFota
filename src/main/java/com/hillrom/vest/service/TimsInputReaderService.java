0edaba269d56e56dfe1f63b31e634009ded3345c
Abhishek Tiwary <abhishek.tiwari@Lnttechservices.com>
2017-07-12 15:14:42 +0530

    fixing the double quotes issue in Tims

--- src/main/java/com/hillrom/vest/service/TimsInputReaderService.java
+++ src/main/java/com/hillrom/vest/service/TimsInputReaderService.java
@@ -85,19 +85,19 @@ public class TimsInputReaderService {
 		    	log.debug("Inside VEST loop ");
 		    	timsService.CASE1_NeitherPatientNorDeviceExist_VEST(record);
 		    	//timsService.CASE2_PatientExistsWithNODevice_VEST(record);
-		    /*	timsService.CASE3_PatientHasMonarchAddVisivest_VEST(record);
+		    	timsService.CASE3_PatientHasMonarchAddVisivest_VEST(record);
 		    	timsService.CASE4_PatientHasDifferentVisivestSwap_VEST(record);
-		    	timsService.CASE5_DeviceOwnedByShell_VEST(record);*/
+		    	timsService.CASE5_DeviceOwnedByShell_VEST(record);
 		    	//timsService.CASE6_DeviceOwnedByDifferentPatient_VEST(record);
 		    	//timsService.CASE7_DeviceIsOrphanPatientDoesNotExist_VEST(record);
 		    	//timsService.CASE8_DeviceIsOrphanButPatientExist_VEST(record);
 		    	//timsService.CASE9_PatientHasDifferentVisivestSwap_VEST(record);
-		    	//timsService.CASE10_PatientHasMonarchAddVisivest_VEST(record);
+		    	timsService.CASE10_PatientHasMonarchAddVisivest_VEST(record);
 		    	//timsService.CASE11_PatientExistsWithNODevice_VEST(record);
 		    	
 		    }
 
-		    /*if(record.getDevice_type().equalsIgnoreCase("MONARCH")){
+		    if(record.getDevice_type().equalsIgnoreCase("MONARCH")){
 		    	log.debug("Inside MONARCH loop ");
 		    	timsService.CASE1_NeitherPatientNorDeviceExist_MONARCH(record);
 		    	//timsService.CASE2_PatientExistsWithNODevice_MONARCH(record);
@@ -111,8 +111,7 @@ public class TimsInputReaderService {
 		    	timsService.CASE10_PatientHasVisivestAddMonarch_MONARCH(record);
 		    	//timsService.CASE11_PatientExistsWithNODevice_MONARCH(record);		    	
 		    	
-		    }*/
-		    
+		    }
 		}
 		
 	}
@@ -122,9 +121,8 @@ public class TimsInputReaderService {
 	{
 
 
-	      //String csvFile = Constants.TIMS_CSV_FILE_PATH + "flat file.csv";
-		    String csvFile = "C:/flatfile.csv";
-	        log.debug("Started reading flat file : " + csvFile);
+	        String csvFile = Constants.TIMS_CSV_FILE_PATH + "flat file.csv";
+		    log.debug("Started reading flat file : " + csvFile);
 	        String line = "";
 	        String cvsSplitBy = ",";
 	        String Outdata = "";

3f331c984cdf839a08ed0c4abb2bbdadd9a3dc12
Karthik Achar <Karthik.Achar_ext@Lnttechservices.com>
2017-11-10 19:54:43 +0530

    modified code changes

--- src/main/java/com/hillrom/vest/repository/AdvancedSearchRepository.java
+++ src/main/java/com/hillrom/vest/repository/AdvancedSearchRepository.java
@@ -81,8 +81,8 @@ public class AdvancedSearchRepository {
 											/*Query formation
 											 * filter: used to append the values from AdvancedClinicDTO
 											 */
-											if(!StringUtils.isBlank(advancedClinicDTO.getName())){
-												filter = filter.append("clinic.name like").append(" '%").append(advancedClinicDTO.getName()).append("%' "); 
+											if(!StringUtils.isBlank(advancedClinicDTO.getClinicName())){
+												filter = filter.append("clinic.name like").append(" '%").append(advancedClinicDTO.getClinicName()).append("%' "); 
 											}
 										    
 											filter= (filter.length()>0)&&(!StringUtils.isBlank(advancedClinicDTO.getClinicType())) ? (filter.append(" AND ")) : (filter.append(""));
@@ -98,9 +98,9 @@ public class AdvancedSearchRepository {
 												}
 											}
 											
-											filter= (filter.length()>0)&&(!StringUtils.isBlank(advancedClinicDTO.getSpecialty())) ? (filter.append(" AND ")) : (filter.append(""));
-										    if(!StringUtils.isBlank(advancedClinicDTO.getSpecialty())){
-										    	filter = filter.append("clinic.speciality like").append(" '%").append(advancedClinicDTO.getSpecialty()).append("%' ");
+											filter= (filter.length()>0)&&(!StringUtils.isBlank(advancedClinicDTO.getClinicSpecialty())) ? (filter.append(" AND ")) : (filter.append(""));
+										    if(!StringUtils.isBlank(advancedClinicDTO.getClinicSpecialty())){
+										    	filter = filter.append("clinic.speciality ").append(" '%").append(advancedClinicDTO.getClinicSpecialty()).append("%' ");
 										    }
 										    
 										    filter= (filter.length()>0)&&(!StringUtils.isBlank(advancedClinicDTO.getCountry())) ? (filter.append(" AND ")) : (filter.append(""));
@@ -124,19 +124,19 @@ public class AdvancedSearchRepository {
 										    
 										    filter= (filter.length()>0)&&(!StringUtils.isBlank(advancedClinicDTO.getZipcode())) ? (filter.append(" AND ")) : (filter.append(""));
 										    if(!StringUtils.isBlank(advancedClinicDTO.getZipcode())){
-										    	filter = filter.append("clinic.zipcode like ").append(" '%").append(advancedClinicDTO.getZipcode()).append("%' ");
+										    	filter = filter.append("clinic.zipcode = ").append(advancedClinicDTO.getZipcode());
 										    }
 										    
 										    
-										    filter= (filter.length()>0)&&(!StringUtils.isBlank(advancedClinicDTO.getStatus())) ? (filter.append(" AND ")) : (filter.append(""));
-										    if(!StringUtils.isBlank(advancedClinicDTO.getStatus())){
-										    	if(advancedClinicDTO.getStatus().equalsIgnoreCase("All"))
+										    filter= (filter.length()>0)&&(!StringUtils.isBlank(advancedClinicDTO.getClinicStatus())) ? (filter.append(" AND ")) : (filter.append(""));
+										    if(!StringUtils.isBlank(advancedClinicDTO.getClinicStatus())){
+										    	if(advancedClinicDTO.getClinicStatus().equalsIgnoreCase("All"))
 										    	{
-										    		filter = filter.append("clinic.is_deleted like").append(" '%%' ");
+										    		filter = filter.append("clinic.is_deleted IN(0,1)");
 										    	}
-										    	else if(advancedClinicDTO.getStatus().equalsIgnoreCase("Active"))
+										    	else if(advancedClinicDTO.getClinicStatus().equalsIgnoreCase("Active"))
 										    	filter = filter.append("clinic.is_deleted =").append(" 0 ");
-										    	else if(advancedClinicDTO.getStatus().equalsIgnoreCase("Inactive"))
+										    	else if(advancedClinicDTO.getClinicStatus().equalsIgnoreCase("Inactive"))
 											    	filter = filter.append("clinic.is_deleted =").append(" 1 ");
 										    }
 										    

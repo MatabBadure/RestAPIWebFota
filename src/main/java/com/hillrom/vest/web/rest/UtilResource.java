efd4df8679415ae638a498dec6358380676195a3
Karthik Achar <Karthik.Achar_ext@Lnttechservices.com>
2017-11-15 21:09:11 +0530

    added diagnostic type code description

--- src/main/java/com/hillrom/vest/web/rest/UtilResource.java
+++ src/main/java/com/hillrom/vest/web/rest/UtilResource.java
@@ -2,7 +2,6 @@ package com.hillrom.vest.web.rest;
 
 import java.util.List;
 
-import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 
 import net.minidev.json.JSONObject;
@@ -15,11 +14,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hillrom.vest.domain.HillromTypeCodeFormat;
 import com.hillrom.vest.exceptionhandler.HillromException;
 import com.hillrom.vest.repository.HillromTypeCodeFormatRepository;
-import com.hillrom.vest.security.AuthoritiesConstants;
 import com.hillrom.vest.util.MessageConstants;
 
 
@@ -50,6 +50,26 @@ public class UtilResource {
         	jsonObject.put("message", MessageConstants.HR_312);
         }else{        
         	jsonObject.put("message", MessageConstants.HR_311);
+        	jsonObject.put("typeCode", typeCodeList);
+        }
+		return new ResponseEntity<JSONObject>(jsonObject, HttpStatus.OK);
+    }
+    
+    /**
+     * GET  /patient/diagnosis -> get all the diagnosis from type code or description.
+     */
+    @RequestMapping(value = "patient/diagnosis",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JSONObject> getDiagnosticTypeCode(@RequestParam String searchString) throws HillromException {
+        log.debug("REST request to get List of Patient Diagnosis : {}", searchString);
+        JSONObject jsonObject = new JSONObject();
+        searchString = "%"+searchString+"%";
+        List<HillromTypeCodeFormat> typeCodeList = hillromTypeCodeFormatRepository.findDiagnosisTypeCode(searchString);
+        if(typeCodeList.isEmpty()){
+        	jsonObject.put("message", MessageConstants.HR_312);
+        }else{        
+       	jsonObject.put("message", MessageConstants.HR_311);
         	jsonObject.put("typeCode", typeCodeList);
         }
 		return new ResponseEntity<JSONObject>(jsonObject, HttpStatus.OK);

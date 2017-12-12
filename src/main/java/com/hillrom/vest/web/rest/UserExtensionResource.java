52e11b229658e236b1cd6b2eeed6156a5002808f
zebatahseen <Zeba.Tahseen@Lnttechservices.com>
2017-12-12 16:40:42 +0530

    code change for hcp search

--- src/main/java/com/hillrom/vest/web/rest/UserExtensionResource.java
+++ src/main/java/com/hillrom/vest/web/rest/UserExtensionResource.java
@@ -4,6 +4,7 @@ import static com.hillrom.vest.config.Constants.VEST;
 import static com.hillrom.vest.config.Constants.MONARCH;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -33,6 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hillrom.monarch.service.PatientNoEventMonarchService;
 import com.hillrom.vest.config.Constants;
+import com.hillrom.vest.domain.Clinic;
 import com.hillrom.vest.domain.PatientInfo;
 import com.hillrom.vest.domain.User;
 import com.hillrom.vest.domain.UserExtension;
@@ -302,19 +304,25 @@ public class UserExtensionResource {
             @RequestParam(value = "sort_by", required = false) String sortBy,
             @RequestParam(value = "asc",required = false) Boolean isAscending)
         throws URISyntaxException {
-    	if(searchString.endsWith("_")){
-    		   searchString = searchString.replace("_", "\\\\_");
+    	if(!StringUtils.isBlank(searchString)) {
+    		if(searchString.endsWith("_")){
+    			searchString = searchString.replace("_", "\\\\_");
+    		}
+    		String queryString = new StringBuilder("'%").append(searchString).append("%'").toString();
+    		Map<String,Boolean> sortOrder = new HashMap<>();
+    		if(sortBy != null  && !sortBy.equals("")) {
+    			isAscending =  (isAscending != null) ?  isAscending : true;
+    			sortOrder.put(sortBy, isAscending);
+    		}
+    		Page<HcpVO> page = userSearchRepository.findHCPBy(queryString,filter,
+    				PaginationUtil.generatePageRequest(offset, limit),sortOrder, associatedToClinicId);
+    		HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/user/hcp/search", offset, limit);
+    		return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     	}
-    	String queryString = new StringBuilder("'%").append(searchString).append("%'").toString();
-    	Map<String,Boolean> sortOrder = new HashMap<>();
-    	if(sortBy != null  && !sortBy.equals("")) {
-    		isAscending =  (isAscending != null) ?  isAscending : true;
-    		sortOrder.put(sortBy, isAscending);
+    	else{
+    		List<Clinic> emptyList = new ArrayList<Clinic>();
+    		return new ResponseEntity<>(emptyList,HttpStatus.OK); 
     	}
-    	Page<HcpVO> page = userSearchRepository.findHCPBy(queryString,filter,
-    			PaginationUtil.generatePageRequest(offset, limit),sortOrder, associatedToClinicId);
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/user/hcp/search", offset, limit);
-        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
     
     /**

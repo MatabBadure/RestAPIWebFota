b26079e17504ef32b3bbf3b69ae19c4ab0738f49
LCLADMIN <LCLADMIN>
2018-02-02 15:22:55 +0530

    FOTA CR Bug Fix

--- src/main/java/com/hillrom/vest/repository/FOTA/FOTADeviceRepository.java
+++ src/main/java/com/hillrom/vest/repository/FOTA/FOTADeviceRepository.java
@@ -7,7 +7,10 @@ import org.springframework.data.repository.query.Param;
 import com.hillrom.vest.domain.FOTA.FOTADeviceFWareUpdate;
 
 public interface FOTADeviceRepository extends JpaRepository<FOTADeviceFWareUpdate, Long> {
-	@Query(nativeQuery=true,value= "SELECT * FROM FOTA_DEVICE_FWARE_UPDATE_LOG where device_serial_number =:deviceSerialNumber AND downloaded_status =:inprogressList")
+	/*@Query(nativeQuery=true,value= "SELECT * FROM FOTA_DEVICE_FWARE_UPDATE_LOG where device_serial_number =:deviceSerialNumber AND downloaded_status =:inprogressList AND fota_info_id =:fotaInfoId")
 	FOTADeviceFWareUpdate getFOTADeviceFWwareDetailsByDevSN(
-			@Param("deviceSerialNumber") String deviceSerialNumber, @Param("inprogressList") String inprogressList);
+			@Param("deviceSerialNumber") String deviceSerialNumber, @Param("inprogressList") String inprogressList,@Param("fotaInfoId") Long fotaInfoId);*/
+	@Query(nativeQuery=true,value= "SELECT * FROM FOTA_DEVICE_FWARE_UPDATE_LOG where id =:deviceId AND downloaded_status =:inprogressList")
+	FOTADeviceFWareUpdate getFOTADeviceFWwareDetailsByDevSN(
+			@Param("deviceId") Long deviceId, @Param("inprogressList") String inprogressList);
 }
